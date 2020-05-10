d1b995c3ce3344d3fa1865e70c608d91128ee959
dazedcat19 <59296763+dazedcat19@users.noreply.github.com>
2020-05-10 15:23:12 +0800

    Update checkantibot.lua

--- lua/websitebypass/checkantibot.lua
+++ lua/websitebypass/checkantibot.lua
@@ -1,6 +1,5 @@
-if http.ResultCode < 500 then return false end
-if not http.Headers.Values['Content-Type']:find('text/html') then return false end
-if http.Document.ToString():find('name="jschl_vc"') then
-  return true
-end
+if HTTP.ResultCode < 500 then return false end
+if not HTTP.Headerss.Values['Content-Type']:find('text/html') then return false end
+-- cloudflare
+if HTTP.Document.ToString():find('name="jschl_vc"') then return true, 'cf' end
 return false
