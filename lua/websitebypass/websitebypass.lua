e99b459f03e6c3ec1af775a684bc6315cc85b127
dazedcat19 <59296763+dazedcat19@users.noreply.github.com>
2020-05-24 02:24:37 +0800

    Update websitebypass.lua

--- lua/websitebypass/websitebypass.lua
+++ lua/websitebypass/websitebypass.lua
@@ -1,7 +1,7 @@
 -- don't change the function name(hardcoded, case sensitive)
 -- don't change the first 2 parameters method and url(hardcoded)
 -- must return 1 boolean value
-function ____WebsiteBypass(METHOD, URL, srv)
+function ____WebsiteBypass(METHOD, URL)
 	local bypass
 	if HTTP.Headers.Values['Server']:lower():find('cloudflare') then
 		bypass = require 'websitebypass.cloudflare'
