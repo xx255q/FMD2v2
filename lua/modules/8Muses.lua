c7d63173e50f2a9e9e5b917a19dfcf5985fc69d3
dazedcat19 <59296763+dazedcat19@users.noreply.github.com>
2020-06-01 07:52:40 +0800

    Update 8Muses.lua

--- lua/modules/8Muses.lua
+++ lua/modules/8Muses.lua
@@ -2,7 +2,7 @@ function Init()
 	local m = NewWebsiteModule()
 	m.ID                         = 'd5d532724aba452ea6d4b50663b76326'
 	m.Name                       = '8Muses'
-	m.RootURL                    = 'https://www.8muses.com'
+	m.RootURL                    = 'https://comics.8muses.com'
 	m.OnGetInfo                  = 'GetInfo'
 	m.OnGetPageNumber            = 'GetPageNumber'
 	m.OnGetImageURL              = 'GetImageURL'
@@ -21,24 +21,25 @@ function GetInfo()
 			MANGAINFO.Title = x.XPathString('//*[@class="top-menu-breadcrumb"]//li[last()-1]')
 		end
 		-- multi
+		local a, n
 		if x.XPathString('//*[@class="gallery"]/a') ~= '' then
 			for _, v in ipairs(x.XPathI('//*[@class="gallery"]/a')) do
-				s = v.GetAttribute('href')
-				if s ~= '' then
-					MANGAINFO.ChapterLinks.Add(s)
-					s = v.ToString()
-					if (MANGAINFO.Title ~= '') and s:find('[iI][sS][sS][uU][eE]') then
-						s = MANGAINFO.Title .. '-' .. s
+				a = v.GetAttribute('href')
+				if a ~= '' then
+					n = v.ToString()
+					if (MANGAINFO.Title ~= '') and n:find('[iI][sS][sS][uU][eE]') then
+						n = MANGAINFO.Title .. '-' .. n
 					end
-					MANGAINFO.ChapterNames.Add(s)
+					MANGAINFO.ChapterLinks.Add(a)
+					MANGAINFO.ChapterNames.Add(n)
 				end
 			end
 		else
 			MANGAINFO.ChapterLinks.Add(MANGAINFO.URL)
 			if s ~= '' then
-				MANGAINFO.ChapterLinks.Add(MANGAINFO.Title .. ' - ' .. s)
+				MANGAINFO.ChapterNames.Add(MANGAINFO.Title .. ' - ' .. s)
 			else
-				MANGAINFO.ChapterLinks.Add(MANGAINFO.Title)
+				MANGAINFO.ChapterNames.Add(MANGAINFO.Title)
 			end
 		end
 		return no_error
@@ -48,9 +49,9 @@ function GetInfo()
 end
 
 function GetPageNumber()
-	if HTTP.GET(RemoveURLDelim(MaybeFillHost(Module.RootURL, URL))) then
+	if HTTP.GET(RemoveURLDelim(MaybeFillHost(MODULE.RootURL, URL))) then
 		local x = TXQuery.Create(HTTP.Document)
-		local s
+		local v, s
 		for _, v in ipairs(x.XPathI('//*[@class="gallery"]/a/div/img/@data-src')) do
 			s = v.ToString()
 			if s ~= '' then
