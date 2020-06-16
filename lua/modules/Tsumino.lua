52fd2db36a5a21422e1da80921f3f6422c6ee5e8
dazedcat19 <59296763+dazedcat19@users.noreply.github.com>
2020-06-16 11:10:39 +0800

    tsumino, fixed all (fixed #26)

--- lua/modules/Tsumino.lua
+++ lua/modules/Tsumino.lua
@@ -2,7 +2,7 @@ function Init()
 	local m = NewWebsiteModule()
 	m.ID                         = '2f5e1b629ce148988e56fca46798afd1'
 	m.Name                       = 'Tsumino'
-	m.RootURL                    = 'http://www.tsumino.com'
+	m.RootURL                    = 'https://www.tsumino.com'
 	m.Category                   = 'H-Sites'
 	m.OnGetDirectoryPageNumber   = 'GetDirectoryPageNumber'
 	m.OnGetNameAndLink           = 'GetNameAndLink'
@@ -10,13 +10,9 @@ function Init()
 	m.OnGetPageNumber            = 'GetPageNumber'
 end
 
-local dirurl = '/Books/Operate'
-local dirurldata = 'PageNumber='
-local dirurldataend = '&Text=&Sort=Newest&List=0&Length=0&MinimumRating=0&ExcludeList=0&CompletelyExcludeHated=false'
-
 function GetDirectoryPageNumber()
-	if HTTP.POST(MODULE.RootURL .. dirurl, dirurldata .. '1' .. dirurldataend) then
-		PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).x.XPathString('json(*)("PageCount")')) or 1
+	if HTTP.POST(MODULE.RootURL .. '/Search/Operate/?type=Book', 'PageNumber=1&Text=&Sort=Newest&List=0&Length=0&MinimumRating=0&ExcludeList=0&CompletelyExcludeHated=false') then
+		PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).x.XPathString('json(*)("pageCount")')) or 1
 		return no_error
 	else
 		return net_problem
@@ -24,10 +20,10 @@ function GetDirectoryPageNumber()
 end
 
 function GetNameAndLink()
-	if HTTP.POST(MODULE.RootURL .. dirurl, dirurldata .. (URL + 1) .. dirurldataend) then
-		local v for v in CreateTXQuery(HTTP.Document).XPath('json(*)("Data")().Entry').Get() do
-			NAMES.Add(v.GetProperty('Title').ToString())
-			LINKS.Add(MODULE.RootURL .. '/Book/Info/' .. v.GetProperty('Id').ToString())
+	if HTTP.POST(MODULE.RootURL .. '/Search/Operate/?type=Book', 'Text=&Sort=Newest&List=0&Length=0&MinimumRating=0&ExcludeList=0&CompletelyExcludeHated=false&PageNumber=' .. (URL + 1)) then
+		local v for v in CreateTXQuery(HTTP.Document).XPath('json(*)("data")().entry').Get() do
+			NAMES.Add(v.GetProperty('title').ToString())
+			LINKS.Add(MODULE.RootURL .. '/Entry/' .. v.GetProperty('id').ToString())
 		end
 		return no_error
 	else
@@ -43,7 +39,7 @@ function GetInfo()
 		MANGAINFO.CoverLink = MaybeFillHost(MODULE.RootURL, x.XPathString('//img[@class="book-page-image img-responsive"]/@src'))
 		MANGAINFO.Title     = x.XPathString('//div[@class="book-line"][starts-with(.,"Title")]/div[@class="book-data"]')
 		MANGAINFO.Artists   = x.XPathString('//div[@class="book-line"][starts-with(.,"Artist")]/div[@class="book-data"]')
-		MANGAINFO.Genres    = x.XPathStringAll('//div[@class="book-line"][starts-with(.,"Parody") or starts-with(.,"Characters") or starts-with(.,"Tags")]/div[@class="book-data"]/*')
+		MANGAINFO.Genres    = x.XPathString('string-join(//div[@class="book-line"]//a[@class="book-tag"],", ")')
 
 		if MANGAINFO.Title ~= '' then
 			MANGAINFO.ChapterLinks.Add(MANGAINFO.URL)
@@ -56,13 +52,16 @@ function GetInfo()
 end
 
 function GetPageNumber()
-	local bookid = URL:match('/info/(%d+)')
+	local bookid = URL:match('(%d+)$')
 	if bookid == nil then return false end
-	HTTP.Headers.Values['Referer'] = ' ' .. MODULE.RootURL .. 'Read/View' .. bookid
-	if HTTP.POST(MODULE.RootURL .. '/Read/Load', 'q=' .. bookid) then
-		local crypto = require 'fmd.crypto'
-		local v for v in CreateTXQuery(HTTP.Document).XPath('json(*).reader_page_urls()').Get() do
-			TASK.PageLinks.Add(MODULE.RootURL .. '/Image/Object?name=' .. crypto.EncodeURLElement(v.ToString()))
+	if HTTP.GET(MODULE.RootURL .. '/Read/Index/' .. bookid .. '?page=1') then
+		local x = CreateTXQuery(HTTP.Document)
+		local data_cdn = x.XPathString('//div[@data-cdn]/@data-cdn')
+		local data_length = tonumber(x.XPathString('//h1[starts-with(.,"Page 1 of ")]'):match('(%d+)$'))
+		if data_cdn and data_length then
+			local i; for i=1, data_length do
+				TASK.PageLinks.Add(data_cdn:gsub('%[PAGE%]',i))
+			end
 		end
 		return true
 	else
