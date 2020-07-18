5538d4a174e431161bac4d49bf9b07a7a22f6ab8
Slasar41 <agungtrianto41@outlook.com>
2020-07-19 02:54:29 +0700

    MethodScans: Added website support

---
+++ lua/modules/MethodScans.lua
@@ -0,0 +1,54 @@
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.Genkan'
+-- DirectoryPagination = '/'   --> Override template variable by uncommenting this line.
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count of the manga list of the current website.
+function GetDirectoryPageNumber()
+	Template.GetDirectoryPageNumber()
+
+	return no_error
+end
+
+-- Get LINKS and NAMES from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return no_error
+end
+
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '756a4f2b20d24e1683b94b7543e4a80a'
+	m.Name                     = 'MethodScans'
+	m.RootURL                  = 'https://methodscans.com'
+	m.Category                 = 'English-Scanlation'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
