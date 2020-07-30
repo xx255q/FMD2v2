bdb4399cd878993ab79811625d5069c71aa31baa
Slasar41 <agungtrianto41@outlook.com>
2020-07-30 16:38:03 +0700

    WhimSubs: Added website support (Fixed #79)

--- lua/modules/ComiCake.lua
+++ lua/modules/ComiCake.lua
@@ -1,41 +1,22 @@
-function getinfo()
-	MANGAINFO.URL=MaybeFillHost(MODULE.RootURL, URL)
-	if HTTP.GET(MANGAINFO.URL) then
-		x=CreateTXQuery(HTTP.Document)
-		MANGAINFO.Title=x.XPathString('//h1/text()')
-		MANGAINFO.CoverLink=x.XPathString('//main//img/@src')
-		MANGAINFO.Artists=x.XPathStringAll('//main//table//tr[td="Artist:"]/td/a')
-		MANGAINFO.Authors=x.XPathStringAll('//main//table//tr[td="Author:"]/td/a')
-		MANGAINFO.Genres=x.XPathStringAll('//main//table//tr[td="Tags:"]/td/a')
-		MANGAINFO.Summary=x.XPathString('//main//pre')
-		x.XPathHREFAll('//main//ul/li/span/a', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
-		MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
-		return no_error
-	else
-		return net_problem
+function Init()
+	function AddWebsiteModule(id, name, url, category)
+		local m = NewWebsiteModule()
+		m.ID                        = id
+		m.Name                      = name
+		m.RootURL                   = url
+		m.Category                  = category
+		m.OnGetNameAndLink          = 'GetNameAndLink'
+		m.OnGetInfo                 = 'GetInfo'
+		m.OnGetPageNumber           = 'GetPageNumber'
+		m.OnGetDirectoryPageNumber  = 'GetDirectoryPageNumber'
 	end
+	local cat = 'English-Scanlation'
+	AddWebsiteModule('380e42a9e93e488abcd74cf47b2cf148', 'LetItGoScans', 'https://reader.letitgo.scans.today', cat)
+	AddWebsiteModule('14ac824309034a6495fc4f91873aeb30', 'ProjectTime', 'https://read.ptscans.com', cat)
+	AddWebsiteModule('02a1221995a54021b084687364cbff50', 'WhimSubs', 'https://whimsubs.xyz', cat)
 end
 
-function getpagenumber()
-	if HTTP.GET(MaybeFillHost(MODULE.RootURL,URL..'/manifest.json')) then
-		local x=CreateTXQuery(HTTP.Document)
-		x.XPathStringAll('json(*).readingOrder().href',TASK.PageLinks)
-	else
-		return false
-	end
-	return true
-end
-
-function getdirectorypagenumber()
-	if HTTP.GET(MODULE.RootURL..'/directory/') then
-		PAGENUMBER=tonumber(CreateTXQuery(HTTP.Document).XPathString('//div[@class="pagination"]//a[contains(., "last")]/@href'):match('/(%d+)/'))
-		return no_error
-	else
-		return net_problem
-	end
-end
-
-function getnameandlink()
+function GetNameAndLink()
 	if HTTP.GET(MODULE.RootURL..'/directory/'..(URL + 1)) then
 		CreateTXQuery(HTTP.Document).XPathHREFAll('//div[@class="mdc-card__media-title"]/a',LINKS,NAMES)
 		return no_error
@@ -44,19 +25,40 @@ function getnameandlink()
 	end
 end
 
-function Init()
-	function AddWebsiteModule(id, name, URL, category)
-		local m = NewWebsiteModule()
-		m.ID = id
-		m.Name = name
-		m.RootURL = URL
-		m.Category = category
-		m.OnGetInfo = 'getinfo'
-		m.OnGetPageNumber = 'getpagenumber'
-		m.OnGetDirectoryPageNumber = 'getdirectorypagenumber'
-		m.OnGetNameAndLink = 'getnameandlink'
+function GetInfo()
+	MANGAINFO.URL = MaybeFillHost(MODULE.RootURL, URL)
+	if HTTP.GET(MANGAINFO.URL) then
+		local x = CreateTXQuery(HTTP.Document)
+		MANGAINFO.Title     = x.XPathString('//h1/text()')
+		MANGAINFO.CoverLink = x.XPathString('//main//img/@src')
+		MANGAINFO.Artists   = x.XPathStringAll('//main//table//tr[td="Artist:"]/td/a')
+		MANGAINFO.Authors   = x.XPathStringAll('//main//table//tr[td="Author:"]/td/a')
+		MANGAINFO.Genres    = x.XPathStringAll('//main//table//tr[td="Tags:"]/td/a')
+		MANGAINFO.Summary   = x.XPathString('//main//pre')
+		
+		x.XPathHREFAll('//main//ul/li/span/a', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
+		MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
+		return no_error
+	else
+		return net_problem
 	end
-	local cat = 'English-Scanlation'
-	AddWebsiteModule('380e42a9e93e488abcd74cf47b2cf148', 'LetItGoScans', 'https://reader.letitgo.scans.today', cat)
-	AddWebsiteModule('14ac824309034a6495fc4f91873aeb30', 'ProjectTime', 'https://read.ptscans.com', cat)
+end
+
+function GetPageNumber()
+	if HTTP.GET(MaybeFillHost(MODULE.RootURL,URL..'/manifest.json')) then
+		local x = CreateTXQuery(HTTP.Document)
+		x.XPathStringAll('json(*).readingOrder().href',TASK.PageLinks)
+	else
+		return false
+	end
+	return true
+end
+
+function GetDirectoryPageNumber()
+	if HTTP.GET(MODULE.RootURL..'/directory/') then
+		PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('//div[@class="pagination"]//a[contains(., "last")]/@href'):match('/(%d+)/'))
+		return no_error
+	else
+		return net_problem
+	end
 end
