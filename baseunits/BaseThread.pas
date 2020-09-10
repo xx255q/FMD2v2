490646b6f6cb00778b9b50eb0521cc473c3e17f5
dazedcat19 <59296763+dazedcat19@users.noreply.github.com>
2020-09-10 08:06:03 +0800

    basethread: moved oncustomterminate to terminatedset

--- baseunits/BaseThread.pas
+++ baseunits/BaseThread.pas
@@ -16,10 +16,12 @@ type
     FOnCustomTerminate: TNotifyEvent;
     function GetTerminated: Boolean;
     procedure CallOnCustomTerminate; inline;
+  protected
+    procedure TerminatedSet; override;
   public
     constructor Create(CreateSuspended: Boolean = True);
     destructor Destroy; override;
-    procedure Terminate;
+
     property IsTerminated: Boolean read GetTerminated;
     property OnCustomTerminate: TNotifyEvent read FOnCustomTerminate write FOnCustomTerminate;
   end;
@@ -38,6 +40,13 @@ begin
   FOnCustomTerminate(Self);
 end;
 
+procedure TBaseThread.TerminatedSet;
+begin
+  inherited TerminatedSet;
+  if Assigned(FOnCustomTerminate) then
+    FOnCustomTerminate(Self);
+end;
+
 constructor TBaseThread.Create(CreateSuspended: Boolean);
 begin
   inherited Create(CreateSuspended);
@@ -47,13 +56,6 @@ end;
 destructor TBaseThread.Destroy;
 begin
   inherited Destroy;
-end;
-
-procedure TBaseThread.Terminate;
-begin
-  inherited Terminate;
-  if Assigned(FOnCustomTerminate) then
-    FOnCustomTerminate(Self);
 end;
 
 end.
