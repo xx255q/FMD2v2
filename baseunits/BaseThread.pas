9dd040f814dcf1ccb1fabb628007174d55f08071
dazedcat19 <59296763+dazedcat19@users.noreply.github.com>
2020-09-15 07:52:48 +0800

    basethread: hide terminate again. some race condition ocurred.

--- baseunits/BaseThread.pas
+++ baseunits/BaseThread.pas
@@ -16,12 +16,10 @@ type
     FOnCustomTerminate: TNotifyEvent;
     function GetTerminated: Boolean;
     procedure CallOnCustomTerminate; inline;
-  protected
-    procedure TerminatedSet; override;
   public
     constructor Create(CreateSuspended: Boolean = True);
     destructor Destroy; override;
-
+    procedure Terminate;
     property IsTerminated: Boolean read GetTerminated;
     property OnCustomTerminate: TNotifyEvent read FOnCustomTerminate write FOnCustomTerminate;
   end;
@@ -40,13 +38,6 @@ begin
   FOnCustomTerminate(Self);
 end;
 
-procedure TBaseThread.TerminatedSet;
-begin
-  inherited TerminatedSet;
-  if Assigned(FOnCustomTerminate) then
-    FOnCustomTerminate(Self);
-end;
-
 constructor TBaseThread.Create(CreateSuspended: Boolean);
 begin
   inherited Create(CreateSuspended);
@@ -56,6 +47,13 @@ end;
 destructor TBaseThread.Destroy;
 begin
   inherited Destroy;
+end;
+
+procedure TBaseThread.Terminate;
+begin
+  inherited Terminate;
+  if Assigned(FOnCustomTerminate) then
+    FOnCustomTerminate(Self);
 end;
 
 end.
