7f9585750fe4d86e8029bbd80ae0a24391219dca
dazedcat19 <59296763+dazedcat19@users.noreply.github.com>
2020-10-07 09:52:31 +0800

    batoto: removed templates.anyacg. other websites are gone. fixed info and download (fixed #133)

--- lua/modules/Batoto.lua
+++ lua/modules/Batoto.lua
@@ -1,43 +1,4 @@
 ----------------------------------------------------------------------------------------------------
--- Local Constants
-----------------------------------------------------------------------------------------------------
-
-local Template = require 'templates.AnyACG'
--- DirectoryPagination = '/'   --> Override template variable by uncommenting this line.
-
-----------------------------------------------------------------------------------------------------
--- Event Functions
-----------------------------------------------------------------------------------------------------
-
--- Get info and chapter list for current manga.
-function GetInfo()
-	Template.GetInfo()
-
-	return no_error
-end
-
--- Get the page count of the manga list of the current website.
-function GetDirectoryPageNumber()
-	Template.GetDirectoryPageNumber()
-
-	return no_error
-end
-
--- Get LINKS and NAMES from the manga list of the current website.
-function GetNameAndLink()
-	Template.GetNameAndLink()
-
-	return no_error
-end
-
--- Get the page count for the current chapter.
-function GetPageNumber()
-	Template.GetPageNumber()
-
-	return no_error
-end
-
-----------------------------------------------------------------------------------------------------
 -- Module Initialization
 ----------------------------------------------------------------------------------------------------
 
@@ -51,4 +12,91 @@ function Init()
 	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+DirectoryPagination = '/browse?sort=title&page='
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	if not HTTP.GET(MaybeFillHost(MODULE.RootURL, URL)) then return net_problem end
+
+	local x = CreateTXQuery(HTTP.Document)
+	MANGAINFO.Title     = x.XPathString('//h3[@class="item-title"]/a') .. GetLanguageCodeSuffix(x.XPathString('//h3[@class="item-title"]/parent::*/span[contains(@class, "flag")]/@class'))
+	MANGAINFO.CoverLink = x.XPathString('//div[contains(@class, "attr-cover")]/img/@src')
+	MANGAINFO.Authors   = x.XPathStringAll('//div[@class="attr-item" and (./b="Authors:")]/span/a')
+	MANGAINFO.Genres    = x.XPathStringAll('//div[@class="attr-item" and (./b="Genres:")]/span/span')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//div[@class="attr-item" and (./b="Release status:")]/span'))
+	MANGAINFO.Summary   = x.XPathString('//h5/following-sibling::pre')
+
+	x.XPathHREFAll('//div[contains(@class, "chapter-list")]/div[@class="main"]/div/a', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
+	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
+
+	return no_error
+end
+
+-- Get the page count of the manga list of the current website.
+function GetDirectoryPageNumber()
+	if not HTTP.GET(MODULE.RootURL .. DirectoryPagination .. "1") then return net_problem end
+
+	PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('(//ul[contains(@class, "pagination")])[1]/li[last()-1]')) or 1
+
+	return no_error
+end
+
+-- Get LINKS and NAMES from the manga list of the current website.
+function GetNameAndLink()
+	if not HTTP.GET(MODULE.RootURL .. DirectoryPagination .. (URL + 1)) then return net_problem end
+
+	local x = CreateTXQuery(HTTP.Document)
+	local v for v in x.XPath('//div[@id="series-list"]/div/div').Get() do
+		LINKS.Add(x.XPathString('a/@href', v))
+		NAMES.Add(x.XPathString('a', v) .. GetLanguageCodeSuffix(x.XPathString('span[contains(@class, "flag")]/@class', v)))
+	end
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	if not HTTP.GET(MaybeFillHost(MODULE.RootURL, URL)) then return net_problem end
+	
+	local x = CreateTXQuery(HTTP.Document)
+	local script = x.XPathString('//script[contains(.,"const batojs")]');
+	local server = require("fmd.duktape").ExecJS(script .. [[
+
+var CryptoJS = require("utils/crypto-js.min.js");
+JSON.parse(CryptoJS.AES.decrypt(server, batojs).toString(CryptoJS.enc.Utf8));
+
+]])
+	if server:find('^//') then server = 'https:' .. server end
+	local images = script:match('const images = %[([^%]]+)')
+	local i for i in images:gmatch('"([^",]+)') do
+		TASK.PageLinks.Add(server..i)
+	end
+
+	return no_error
+end
+
+----------------------------------------------------------------------------------------------------
+-- Helper Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get the language suffix by given flag.
+function GetLanguageCodeSuffix(s)
+	local suffix = ' [EN]'
+
+	if s and (s ~= '') then
+		s = s:match('flag_(%S+)')
+		if s and (s ~= 'united_kingdom') then suffix = ' [' .. string.upper(s) .. ']' end
+	end
+
+	return suffix
 end
