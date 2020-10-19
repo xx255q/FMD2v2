e2f9c86414089626a37cc44cefbf1ee136daeae4
Slasar41 <agungtrianto41@outlook.com>
2020-10-20 02:03:40 +0700

    9ekor: Fixed all

--- lua/modules/9ekor.lua
+++ lua/modules/9ekor.lua
@@ -1,4 +1,19 @@
 ----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '09c45865698743908b84ecadc71e278c'
+	m.Name                     = '9ekor'
+	m.RootURL                  = 'https://9ekor.com'
+	m.Category                 = 'Indonesian'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
@@ -17,21 +32,9 @@ function GetInfo()
 	if not HTTP.GET(u) then return net_problem end
 
 	x = CreateTXQuery(HTTP.Document)
-	MANGAINFO.Title     = Trim(x.XPathString('//title/substring-after(substring-before(., "Bahasa Indonesia - 9ekor"), "Baca")'))
+	MANGAINFO.Title     = Trim(x.XPathString('//title/substring-before(., "Online - 9Ekor")'))
 
-	pages = tonumber(x.XPathString('//div[@class="pagination"]/a[last()]/@href[not(parent::span/@id="tie-next-page")]'):match('/page/(%d+)/'))
-	if pages == nil then pages = 1 end
-	while true do
-		x.XPathHREFAll('//div[contains(@class, "post-listing")]//h2[@class="post-box-title"]/a', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
-		p = p + 1
-		if p > pages then
-			break
-		elseif HTTP.GET(u .. 'page/' .. tostring(p) .. '/') then
-			x = CreateTXQuery(HTTP.Document)
-		else
-			break
-		end
-	end
+	x.XPathHREFAll('//div[@class="page-content"]//h3[@class="entry-title"]/a', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
 	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
 	return no_error
@@ -45,7 +48,7 @@ function GetNameAndLink()
 	if not HTTP.GET(u) then return net_problem end
 
 	x = CreateTXQuery(HTTP.Document)
-	x.XPathHREFAll('//a[contains(@class, "cvplbd")]', LINKS, NAMES)
+	x.XPathHREFAll('//div[@class="letter-section"]//li/a', LINKS, NAMES)
 
 	return no_error
 end
@@ -58,22 +61,7 @@ function GetPageNumber()
 	if not HTTP.GET(u) then return net_problem end
 
 	x = CreateTXQuery(HTTP.Document)
-	x.XPathStringAll('//div[@id="all"]/img/@src', TASK.PageLinks)
+	x.XPathStringAll('//div[@class="page-content"]//img/@src', TASK.PageLinks)
 
 	return no_error
-end
-
-----------------------------------------------------------------------------------------------------
--- Module Initialization
-----------------------------------------------------------------------------------------------------
-
-function Init()
-	local m = NewWebsiteModule()
-	m.ID                       = '09c45865698743908b84ecadc71e278c'
-	m.Name                     = '9ekor'
-	m.RootURL                  = 'https://9ekor.com'
-	m.Category                 = 'Indonesian'
-	m.OnGetInfo                = 'GetInfo'
-	m.OnGetNameAndLink         = 'GetNameAndLink'
-	m.OnGetPageNumber          = 'GetPageNumber'
 end
