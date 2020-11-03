c2fc9b27715fe6876142f37231aa4776b3b0ea90
Slasar41 <agungtrianto41@outlook.com>
2020-11-04 03:48:23 +0700

    Update MangaHasu.lua

--- lua/modules/MangaHasu.lua
+++ lua/modules/MangaHasu.lua
@@ -1,40 +1,17 @@
-function getinfo()
-	MANGAINFO.URL=MaybeFillHost(MODULE.RootURL, URL)
-	if HTTP.GET(MANGAINFO.URL) then
-		local x=CreateTXQuery(HTTP.Document)
-		MANGAINFO.Title=x.XPathString('//div[@class="info-title"]/h1')
-		MANGAINFO.CoverLink=MaybeFillHost(MODULE.RootURL, x.XPathString('//div[contains(@class, "info-img")]/img/@src'))
-		MANGAINFO.Authors=x.XPathStringAll('//div[contains(b, "Author")]/span/a')
-		MANGAINFO.Artists=x.XPathStringAll('//div[contains(b, "Artist")]/span/a')
-		MANGAINFO.Genres=x.XPathStringAll('//div[contains(b, "Genre")]/span/a')
-		MANGAINFO.Status=MangaInfoStatusIfPos(x.XPathString('//div[contains(b, "Status")]/span/a'))
-		MANGAINFO.Summary=x.XPathString('//h3[contains(.,"Summary")]/following-sibling::*')
-		x.XPathHREFAll('//div[@class="list-chapter"]/table//td[@class="name"]/a',MANGAINFO.ChapterLinks,MANGAINFO.ChapterNames)
-		MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
-		return no_error
-	else
-		return net_problem
-	end
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                         = 'e30f500d4eb0413e858dc071616228f1'
+	m.Name                       = 'MangaHasu'
+	m.RootURL                    = 'http://mangahasu.se'
+	m.Category                   = 'English'
+	m.OnGetNameAndLink           = 'GetNameAndLink'
+	m.OnGetDirectoryPageNumber   = 'OnGetDirectoryPageNumber'
+	m.OnGetInfo                  = 'GetInfo'
+	m.OnGetPageNumber            = 'GetPageNumber'
+	m.OnBeforeDownloadImage      = 'BeforeDownloadImage'
 end
 
-function beforedownloadimage()
-	HTTP.Headers.Values['Referer'] = MODULE.RootURL
-	return true
-end
-
-function getpagenumber()
-	TASK.PageLinks.Clear()
-	TASK.PageNumber = 0
-	if HTTP.GET(MaybeFillHost(MODULE.RootURL, URL)) then
-		local x=CreateTXQuery(HTTP.Document)
-		x.XPathStringAll('//div[@class="img"]/img/@src', TASK.PageLinks)
-	else
-		return false
-	end
-	return true
-end
-
-function getnameandlink()
+function GetNameAndLink()
 	if HTTP.GET(MODULE.RootURL .. '/directory.html?page=' .. (URL + 1)) then
 		local x = CreateTXQuery(HTTP.Document)
 		x.XPathHREFAll('//ul[@class="list_manga"]/li//a[@class="name-manga"]', LINKS, NAMES)
@@ -44,7 +21,7 @@ function getnameandlink()
 	end
 end
 
-function getdirectorypagenumber()
+function OnGetDirectoryPageNumber()
 	if HTTP.GET(MODULE.RootURL .. '/directory.html') then
 		x = CreateTXQuery(HTTP.Document)
 		PAGENUMBER = tonumber(x.XPathString('//div[@class="pagination-ct"]/a[last()]/substring-after(@href,"=")')) or 1
@@ -54,15 +31,42 @@ function getdirectorypagenumber()
 	end
 end
 
-function Init()
-	local m = NewWebsiteModule()
-	m.ID = 'e30f500d4eb0413e858dc071616228f1'
-	m.Name = 'MangaHasu'
-	m.RootURL = 'http://mangahasu.se'
-	m.Category = 'English'
-	m.OnGetInfo='getinfo'
-	m.OnGetPageNumber='getpagenumber'
-	m.OnGetNameAndLink='getnameandlink'
-	m.OnBeforeDownloadImage='beforedownloadimage'
-	m.OnGetDirectoryPageNumber='getdirectorypagenumber'
+function GetInfo()
+	MANGAINFO.URL = MaybeFillHost(MODULE.RootURL, URL)
+	if HTTP.GET(MANGAINFO.URL) then
+		local x = CreateTXQuery(HTTP.Document)
+		MANGAINFO.Title     = x.XPathString('//div[@class="info-title"]/h1')
+		MANGAINFO.CoverLink = MaybeFillHost(MODULE.RootURL, x.XPathString('//div[contains(@class, "info-img")]/img/@src'))
+		MANGAINFO.Authors   = x.XPathStringAll('//div[contains(b, "Author")]/span/a')
+		MANGAINFO.Artists   = x.XPathStringAll('//div[contains(b, "Artist")]/span/a')
+		MANGAINFO.Genres    = x.XPathStringAll('//div[contains(b, "Genre")]/span/a')
+		MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//div[contains(b, "Status")]/span/a'))
+		MANGAINFO.Summary   = x.XPathString('//h3[contains(.,"Summary")]/following-sibling::*')
+
+		x.XPathHREFAll('//div[@class="list-chapter"]/table//td[@class="name"]/a',MANGAINFO.ChapterLinks,MANGAINFO.ChapterNames)
+		MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
+
+		HTTP.Reset()
+		HTTP.Headers.Values['Referer'] = MANGAINFO.URL
+		return no_error
+	else
+		return net_problem
+	end
+end
+
+function GetPageNumber()
+	TASK.PageLinks.Clear()
+	TASK.PageNumber = 0
+	if HTTP.GET(MaybeFillHost(MODULE.RootURL, URL)) then
+		local x = CreateTXQuery(HTTP.Document)
+		x.XPathStringAll('//div[@class="img"]/img/@src', TASK.PageLinks)
+	else
+		return false
+	end
+	return true
+end
+
+function BeforeDownloadImage()
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL
+	return true
 end
