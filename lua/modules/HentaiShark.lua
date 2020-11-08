50762d971a83675a76cdd493f22964c5d47cf0f2
Slasar41 <agungtrianto41@outlook.com>
2020-11-09 01:56:17 +0700

    HentaiShark: Added website support
    References: https://github.com/manga-download/hakuneko/issues/1045

---
+++ lua/modules/HentaiShark.lua
@@ -0,0 +1,60 @@
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                         = '43c6880fe2804a198c6e9f748641498f'
+	m.Name                       = 'HentaiShark'
+	m.RootURL                    = 'https://www.hentaishark.com'
+	m.Category                   = 'H-Sites'
+	m.OnGetDirectoryPageNumber   = 'GetDirectoryPageNumber'
+	m.OnGetNameAndLink           = 'GetNameAndLink'
+	m.OnGetInfo                  = 'GetInfo'
+	m.OnGetPageNumber            = 'GetPageNumber'
+end
+
+function GetDirectoryPageNumber()
+	if HTTP.GET(MODULE.RootURL) then
+		PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('//ul[@class="pagination"]/li[last()-1]/a'))
+		return no_error
+	else
+		return net_problem
+	end
+end
+
+function GetNameAndLink()
+	if HTTP.GET(MODULE.RootURL .. '/?page=' .. (URL + 1)) then
+		local x = CreateTXQuery(HTTP.Document)
+		local v; for v in x.XPath('//div[@class="item"]/a').Get() do
+			LINKS.Add(v.GetAttribute('href'))
+			NAMES.Add(x.XPathString('div[@class="caption"]', v))
+		end
+		return no_error
+	else
+		return net_problem
+	end
+end
+
+function GetInfo()
+	MANGAINFO.URL = MaybeFillHost(MODULE.RootURL, URL)
+	if HTTP.GET(MANGAINFO.URL) then
+		local x = CreateTXQuery(HTTP.Document)
+		MANGAINFO.Title      = x.XPathString('//h2[contains(@class,"widget-title")]')
+		MANGAINFO.CoverLink  = x.XPathString('//div[contains(@class,"show-manga")]//img/@src')
+		MANGAINFO.Artists    = x.XPathStringAll('//dt[contains(.,"Artists")]/following-sibling::dd[1]/a')
+		MANGAINFO.Genres     = x.XPathStringAll('//dd[@class="tag-links"]/a/text()[not(parent::span/@class)]')
+
+		MANGAINFO.ChapterLinks.Add(URL)
+		MANGAINFO.ChapterNames.Add(MANGAINFO.Title)
+		return no_error
+	else
+		return net_problem
+	end
+end
+
+function GetPageNumber()
+	if HTTP.GET(MaybeFillHost(MODULE.RootURL, URL)) then
+		local x = CreateTXQuery(HTTP.Document)
+		CreateTXQuery(HTTP.Document).XPathStringAll('//img[contains(@class,"lazy")]/@src/replace(.,"thumb_","")', TASK.PageLinks)
+		return true
+	else
+		return false
+	end
+end
