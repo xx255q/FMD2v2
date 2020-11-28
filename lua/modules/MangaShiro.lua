290419bd4c0fe7dc325c51c25b2bb95978459e5e
OCKEYaNTO <75157616+OCKEYaNTO@users.noreply.github.com>
2020-11-28 02:34:20 -0500

    Update AsuraScans

--- lua/modules/MangaShiro.lua
+++ lua/modules/MangaShiro.lua
@@ -198,7 +198,15 @@ function getpagenumber()
 					string.find(v.GetAttribute('src'), "Iklan.jpg") == nil then
 					TASK.PageLinks.Add(v.GetAttribute('src'))
 				end
-			end	
+			end
+		elseif MODULE.ID == '7103ae6839ea46ec80cdfc2c4b37c803' then -- AsuraScans
+			local v; for v in x.XPath('//*[@id="readerarea"]/p//img').Get() do
+				if string.find(v.GetAttribute('style'), "diplay:none;") == nil and
+					string.find(v.GetAttribute('width'), "1px") == nil and					
+					string.find(v.GetAttribute('src'), "zfRuVsd.jpg") == nil then
+					TASK.PageLinks.Add(v.GetAttribute('src'))
+				end
+			end
 		else
 			if TASK.PageLinks.Count == 0 then x.XPathStringAll('//*[@class="reader-area"]//img/@src', TASK.PageLinks) end
 			if TASK.PageLinks.Count == 0 then x.XPathStringAll('//*[@id="readerarea"]//img/@src', TASK.PageLinks) end
