5ff73d9c0b35c5dc6e3f614468441e5a96b00871
Slasar41 <agungtrianto41@outlook.com>
2021-02-08 05:48:21 +0700

    ImmortalUpdates: Added website support (Closes #317)

--- lua/modules/Madara.lua
+++ lua/modules/Madara.lua
@@ -289,6 +289,7 @@ function Init()
 	AddWebsiteModule('2ee1ccc7bf774f79a4b0debda4bfd867', 'SleepyTranslations', 'https://sleepytranslations.com')
 	AddWebsiteModule('f01040ee781d4ae1929031419b97d2e0', 'VoidScans', 'https://voidscans.com')
 	AddWebsiteModule('598672e8158d4fd781bea8d426534695', 'GourmetScans', 'https://gourmetscans.net')
+	AddWebsiteModule('813f780f2d3b41ab8de577aadcbd4d7a', 'ImmortalUpdates', 'https://immortalupdates.com')
 
 	cat = 'French'
 	AddWebsiteModule('41867fa36f2f49959df9fef8aa53ffb5', 'WakaScan', 'https://wakascan.com')
