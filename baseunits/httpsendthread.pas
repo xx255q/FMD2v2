2e7befc361eae67a20a07b5d16436369f77ade51
Slasar41 <agungtrianto41@outlook.com>
2021-02-24 04:12:41 +0700

    Update httpsendthread.pas

--- baseunits/httpsendthread.pas
+++ baseunits/httpsendthread.pas
@@ -157,7 +157,7 @@ function FormatByteSize(const ABytes: Integer; AShowPerSecond: Boolean = False):
 const
   UserAgentSynapse   = 'Mozilla/4.0 (compatible; Synapse)';
   UserAgentCURL      = 'curl/7.70.0';
-  UserAgentDefault   = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0';
+  UserAgentDefault   = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0';
   HeartBeatRate      = 1000;
 
 var
