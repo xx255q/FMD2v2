7ca9c6257ea9b0de48c2d217da1cbec5dbc93949
rs3mk <rs3mk@eyepaste.com>
2021-05-13 05:28:14 -0500

    RagnarokScan: Added website support (#534)
    Issue #524

--- lua/modules/Madara.lua
+++ lua/modules/Madara.lua
@@ -451,4 +451,5 @@ function Init()
 	cat = 'Spanish'
 	AddWebsiteModule('22c481c279c740b897ffc906ab808502', 'TuMangaNet', 'https://tumanga.net')
 	AddWebsiteModule('89caa2c18f5b40e6a18b7ecc78a0d1c1', 'TopComicPorno', 'https://topcomicporno.com')
+	AddWebsiteModule('36b0f19e144a4642a2839ec9cb728c3a', 'RagnarokScan', 'https://ragnarokscan.com')
 end
