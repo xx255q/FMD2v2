7288dedb03f15bf66dffae1236b84d778e0d8d16
Slasar41 <agungtrianto41@outlook.com>
2021-05-31 15:00:32 +0700

    NhatTruyen: Changed url (Closes #568)

--- lua/modules/TruyenChon.lua
+++ lua/modules/TruyenChon.lua
@@ -15,7 +15,7 @@ function Init()
 	local cat = 'Vietnamese'
 	AddWebsiteModule('ef7f922bd45f4f9d9c559a55f987004d', 'TruyenChon', 'http://truyenchon.com', cat)
 	AddWebsiteModule('567780dbaa3149e7ad698f11ce68ea9b', 'NetTruyen', 'http://www.nettruyen.com', cat)
-	AddWebsiteModule('d25308907620480496bd73f50451d67f', 'NhatTruyen', 'http://nhattruyen.com', cat)
+	AddWebsiteModule('d25308907620480496bd73f50451d67f', 'NhatTruyen', 'http://nhattruyentranh.com', cat)
 
 	cat = 'English'
 	AddWebsiteModule('d2f24dec90e841b1aab4bea145ffb638', 'MangaNT', 'https://mangant.com', cat)
