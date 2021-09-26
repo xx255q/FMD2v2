3e3ad7bce7171a2ea966855f249ecf635f7315bc
Slasar41 <agungtrianto41@outlook.com>
2021-09-27 04:00:59 +0700

    Manhwa18: Fixed download (Closes #742)

--- lua/modules/Lhscans.lua
+++ lua/modules/Lhscans.lua
@@ -166,7 +166,7 @@ function GetPageNumber()
 			x.XPathStringAll('//img[contains(@class, "chapter-img")]/@*[contains(., "https")]', TASK.PageLinks)
 		else
 			x.XPathStringAll('//img[contains(@class, "chapter-img")]/@data-src', TASK.PageLinks)
-			if TASK.PageLinks.Count == 0 then x.XPathStringAll('//img[contains(@class, "chapter-img")]/src', TASK.PageLinks) end
+			if TASK.PageLinks.Count == 0 then x.XPathStringAll('//img[contains(@class, "chapter-img")]/@src', TASK.PageLinks) end
 		end
 	else
 		return false
