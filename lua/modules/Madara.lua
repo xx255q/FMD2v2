72cc867f24af870830e94a9f3cc17e8b3e146b3d
ahmadi3d <ahmadi3d.ali.wcftdas@gmail.com>
2022-01-21 22:31:50 +0330

    MangaBin, Manga1stOnline: Added websites support (#879)

--- lua/modules/Madara.lua
+++ lua/modules/Madara.lua
@@ -321,6 +321,8 @@ function Init()
 	AddWebsiteModule('76b33c241c0d44a6b4a5b8dd86ec7750', 'ManhuaES', 'https://manhuaes.com')
 	AddWebsiteModule('29003d3eb6c348ab8f8d4ed0cb8f7c6f', 'AllTopManga', 'https://alltopmanga.com')
 	AddWebsiteModule('89497fea6b55470c89fac1eb8f75f61a', 'MangaHyphenTx', 'https://manga-tx.com')
+	AddWebsiteModule('e62d472cc686400b91528c5b52ed0452', 'MangaBin', 'https://mangabin.com')
+	AddWebsiteModule('27433894c7594c97ad00ea083947c28c', 'Manga1stOnline', 'https://manga1st.online')
 
 	cat = 'English-Scanlation'
 	AddWebsiteModule('f17a22a1a24640ecb2ae4f51c47a45c8', 'TrashScanlations', 'https://trashscanlations.com')
