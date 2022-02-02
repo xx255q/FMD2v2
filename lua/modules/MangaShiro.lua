73dc2aa37dcef5dde4579a8a36259ac8ce2b4214
ahmadi3d <ahmadi3d.ali.wcftdas@gmail.com>
2022-02-02 22:05:22 +0330

    RealmScans: Added website support (#893)

--- lua/modules/MangaShiro.lua
+++ lua/modules/MangaShiro.lua
@@ -422,6 +422,7 @@ function Init()
 	AddWebsiteModule('275b85bdaafb47fdbc40f51d2bea99e8', 'TheApolloTeam', 'https://theapollo.team')
 	AddWebsiteModule('15fc68c57ce141f497b872af157d72ac', 'ShimadaScans', 'https://shimadascans.com')
 	AddWebsiteModule('f291e782dab54867a26a161934277177', 'ShiniScan', 'https://shiniscan.com')
+	AddWebsiteModule('3593adad980d454abe489c42e7158032', 'RealmScans', 'https://realmscans.com')
 
 	cat = 'Spanish'
 	AddWebsiteModule('41294a121062494489adfa601c442ef8', 'LegionAsia', 'https://legionasia.com')
