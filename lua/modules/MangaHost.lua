06f13fbd5ee2a19e2ebdbb3f52c9fcfd5bc825d2
Slasar41 <agungtrianto41@outlook.com>
2022-04-01 06:14:27 +0700

    MangaHost: Changed domain

--- lua/modules/MangaHost.lua
+++ lua/modules/MangaHost.lua
@@ -1,4 +1,20 @@
 ----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '47a611b9efb44e34a800bdb0f946ff07'
+	m.Name                     = 'MangaHost'
+	m.RootURL                  = 'https://mangahost4.com'
+	m.Category                 = 'Portuguese'
+	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
@@ -7,31 +23,6 @@ local DirectoryPagination = '/mangas/page/'
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
 ----------------------------------------------------------------------------------------------------
-
--- Get info and chapter list for current manga.
-function GetInfo()
-	local v, x = nil
-	local u = MaybeFillHost(MODULE.RootURL, URL)
-
-	if not HTTP.GET(u) then return net_problem end
-
-	x = CreateTXQuery(HTTP.Document)
-	MANGAINFO.Title     = x.XPathString('//meta[@property="og:title"]/@content')
-	MANGAINFO.CoverLink = x.XPathString('//picture/img/@src')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//div[contains(strong, "Status:")]/text()'), 'Ativo', 'Completo')
-	MANGAINFO.Authors   = x.XPathString('//div[contains(strong, "Autor:")]/text()')
-	MANGAINFO.Artists   = x.XPathString('//div[contains(strong, "Desenho (Art):")]/text()')
-	MANGAINFO.Genres    = x.XPathStringAll('//div[@class="w-col w-col-7"]/article/div[@class="tags"]/a')
-	MANGAINFO.Summary   = Trim(x.XPathString('//div[@class="paragraph"]'))
-
-	for v in x.XPath('//div[@class="card pop"]').Get() do
-		MANGAINFO.ChapterLinks.Add(x.XPathString('div[2]/div/a/@href', v))
-		MANGAINFO.ChapterNames.Add(x.XPathString('div[1]/text()', v) .. ' ' .. x.XPathString('div[1]/span/text()', v))
-	end
-	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
-
-	return no_error
-end
 
 -- Get the page count of the manga list of the current website.
 function GetDirectoryPageNumber()
@@ -57,6 +48,31 @@ function GetNameAndLink()
 	return no_error
 end
 
+-- Get info and chapter list for current manga.
+function GetInfo()
+	local v, x = nil
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	MANGAINFO.Title     = x.XPathString('//meta[@property="og:title"]/@content')
+	MANGAINFO.CoverLink = x.XPathString('//div[@class="widget"]/picture/img/@src')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//div[contains(strong, "Status:")]/text()'), 'Ativo', 'Completo')
+	MANGAINFO.Authors   = x.XPathString('//div[contains(strong, "Autor:")]/text()')
+	MANGAINFO.Artists   = x.XPathString('//div[contains(strong, "Arte:")]/text()')
+	MANGAINFO.Genres    = x.XPathStringAll('//div[@class="w-col w-col-7"]/article/div[@class="tags"]/a')
+	MANGAINFO.Summary   = Trim(x.XPathString('//div[@class="paragraph"]'))
+
+	for v in x.XPath('//div[@class="card pop"]').Get() do
+		MANGAINFO.ChapterLinks.Add(x.XPathString('div[2]/div/a/@href', v))
+		MANGAINFO.ChapterNames.Add(x.XPathString('div[1]/text()', v) .. ' ' .. x.XPathString('div[1]/span/text()', v))
+	end
+	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
+
+	return no_error
+end
+
 -- Get the page count for the current chapter.
 function GetPageNumber()
 	local x = nil
@@ -69,20 +85,4 @@ function GetPageNumber()
 	x.XPathStringAll('//img/@src', TASK.PageLinks)
 
 	return no_error
-end
-
-----------------------------------------------------------------------------------------------------
--- Module Initialization
-----------------------------------------------------------------------------------------------------
-
-function Init()
-	local m = NewWebsiteModule()
-	m.ID                       = '47a611b9efb44e34a800bdb0f946ff07'
-	m.Name                     = 'MangaHost'
-	m.RootURL                  = 'https://mangahosted.com'
-	m.Category                 = 'Portuguese'
-	m.OnGetInfo                = 'GetInfo'
-	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
-	m.OnGetNameAndLink         = 'GetNameAndLink'
-	m.OnGetPageNumber          = 'GetPageNumber'
 end
