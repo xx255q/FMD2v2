5ac0c111b9908e5dfdab800e12bf701a3ac2a7e1
Slasar41 <agungtrianto41@outlook.com>
2022-09-28 07:21:43 +0700

    MangAs: Fixed chapter list and chapter download (Closes #1094)

--- lua/modules/MangAs.lua
+++ lua/modules/MangAs.lua
@@ -15,22 +15,14 @@ XPathTokenGenres    = 'Género'
 
 -- Get info and chapter list for current manga.
 function GetInfo()
-	local u = MaybeFillHost(MODULE.RootURL, URL)
-
-	if not HTTP.GET(u) then return net_problem end
+	Template.GetInfo()
 
 	local x = CreateTXQuery(HTTP.Document)
-	MANGAINFO.Title     = x.XPathString('(//div[contains(@class, "container")]//h2)[1]')
-	MANGAINFO.CoverLink = x.XPathString('//div[@class="boxed"]/img/@src')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//dt[text()="' .. XPathTokenStatus .. '"]/following-sibling::dd[1]/span'), 'Ongoing', 'Complete')
-	MANGAINFO.Authors   = x.XPathStringAll('//dt[text()="' .. XPathTokenAuthors .. '"]/following-sibling::dd[1]/a')
-	MANGAINFO.Artists   = x.XPathStringAll('//dt[text()="' .. XPathTokenArtists .. '"]/following-sibling::dd[1]/a')
-	MANGAINFO.Genres    = x.XPathStringAll('//dt[text()="' .. XPathTokenGenres .. '" or text()="Demográfico"]/following-sibling::dd[1]/a')
-	MANGAINFO.Summary   = x.XPathString('//div[@class="well"]/p[2]')
-
-	local v; for v in x.XPath('//ul[@class="chapters"]/li/h5/eee').Get() do
-		MANGAINFO.ChapterLinks.Add(x.XPathString('a/@href', v))
-		MANGAINFO.ChapterNames.Add(x.XPathString('normalize-space(.)', v))
+	local chapter = GetBetween('let chapter = ', '[', x.XPathString('//ul[@class="chapters"]'))
+	x.ParseHTML('[' .. GetBetween('var ' .. chapter .. ' = [', ']', x.XPathString('//script[@type="text/javascript"]')) .. ']')
+	local v for v in x.XPath('json(*)()').Get() do
+		MANGAINFO.ChapterLinks.Add(URL .. '/' .. x.XPathString('slug', v))
+		MANGAINFO.ChapterNames.Add(x.XPathString('name', v))
 	end
 	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
@@ -46,22 +38,8 @@ end
 
 -- Get the page count for the current chapter.
 function GetPageNumber()
-	if not HTTP.GET(MaybeFillHost(MODULE.RootURL, URL)) then return net_problem end
-	local body = HTTP.Document.ToString()
-	local pages = body:match('var pages = (%[.-%]);')
-	if pages then
-		local json = require "utils.json"
-		local crypto = require "fmd.crypto"
-		local baseuri = (body:gsub("[\r\n\t]", ""):match("aaaaaarray%.push%('(.-)'") or ''):gsub('/+$','') .. '/'
-		local pages = json.decode(pages)
-		local i, v; for i, v in ipairs(pages) do
-			if v.external == '0' then
-				TASK.PageLinks.Add(baseuri .. v.page_image)
-			else
-				TASK.PageLinks.Add(crypto.DecodeURL(crypto.DecodeBase64(v.page_image:gsub('^https://', ''))))
-			end
-		end
-	end
+	Template.GetPageNumber()
+
 	return no_error
 end
 
