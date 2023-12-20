896a8519bea93469ba821f62835ef93160a1138d
Trollam <Trollam@users.noreply.github.com>
2023-12-20 15:07:08 +0100

    AsuraScans: Changed domain (#1297)
    * fix_1296
    
    * Domain changed.

--- lua/modules/MangaShiro.lua
+++ lua/modules/MangaShiro.lua
@@ -436,7 +436,7 @@ function Init()
 	AddWebsiteModule('0e45db2650604f74a0caeb7c1d69a749', 'SWATManga', 'https://swatmanga.me')
 	
 	cat = 'English-Scanlation'
-	AddWebsiteModule('7103ae6839ea46ec80cdfc2c4b37c803', 'AsuraScans', 'https://www.asurascans.com')
+	AddWebsiteModule('7103ae6839ea46ec80cdfc2c4b37c803', 'AsuraScans', 'https://asuratoon.com')
 	AddWebsiteModule('fb34a56c83f54b19b57a9a92070fe899', 'FlameScans', 'https://flamescans.org')
 	AddWebsiteModule('568f2731188d4f058c5542e89603a030', 'AzureManga', 'https://azuremanga.com')
 	AddWebsiteModule('f8c92a9a83f64deebee5ec58b7b15bdf', 'xCaliBRScans', 'https://xcalibrscans.com')
