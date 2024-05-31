ecb0f62db802d68b2c87f04b0d6da7043b1d48a6
Slasar41 <agungtrianto41@outlook.com>
2024-05-31 21:57:42 +0700

    Hiperdex: Use template

---
+++ lua/modules/Hiperdex.lua
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '2b5f00cfbb124546b6c11a7e5ec9c403'
+	m.Name                     = 'Hiperdex'
+	m.RootURL                  = 'https://hiperdex.top'
+	m.Category                 = 'H-Sites'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.Madara'
+-- XPathTokenAuthors = 'Author(s)'
+-- XPathTokenArtists = 'Artist(s)'
+-- XPathTokenGenres  = 'Genre(s)'
+-- XPathTokenStatus  = 'Status'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return no_error
+end
