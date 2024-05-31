6d388d77f83ca2a2236714bbad4f5262e9188fc6
Slasar41 <agungtrianto41@outlook.com>
2024-05-31 22:01:53 +0700

    GDScans: Use template

---
+++ lua/modules/GDScans.lua
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = 'f1371b4a0cd04254ae4b1f5065a46dc8'
+	m.Name                     = 'GDScans'
+	m.RootURL                  = 'https://gdscans.com'
+	m.Category                 = 'English-Scanlation'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.Madara'
+-- XPathTokenAuthors = 'Author(s)'
+-- XPathTokenArtists = 'Artist(s)'
+-- XPathTokenGenres  = 'Genre(s)'
+-- XPathTokenStatus  = 'Status'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return no_error
+end
