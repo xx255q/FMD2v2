88bf8e4a9af73bdf18b5dcd69e2ff1a39dddc41f
Slasar41 <agungtrianto41@outlook.com>
2024-06-11 09:53:11 +0700

    CopyPasteScanlation: Use template

---
+++ lua/modules/CopyPasteScanlation.lua
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '8ae3c5a7a1924715940594be746448a9'
+	m.Name                     = 'CopyPasteScanlation'
+	m.RootURL                  = 'https://www.cpscan.xyz'
+	m.Category                 = 'Spanish-Scanlation'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.Madara'
+-- XPathTokenAuthors = 'Author(s)'
+-- XPathTokenArtists = 'Artist(s)'
+-- XPathTokenGenres  = 'Genre(s)'
+-- XPathTokenStatus  = 'Status'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return no_error
+end
