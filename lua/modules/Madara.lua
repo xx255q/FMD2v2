7478ce4e8172b64671d82fe57dfa04d99644e010
Slasar41 <agungtrianto41@outlook.com>
2024-06-11 09:57:46 +0700

    RagnarokScan: Use template

--- lua/modules/Madara.lua
+++ lua/modules/Madara.lua
@@ -484,7 +484,6 @@ function Init()
 
 	cat = 'Spanish'
 	AddWebsiteModule('22c481c279c740b897ffc906ab808502', 'TuMangaNet', 'https://tumanga.net')
-	AddWebsiteModule('36b0f19e144a4642a2839ec9cb728c3a', 'RagnarokScan', 'https://ragnarokscan.com')
 	AddWebsiteModule('e4a3a8ed254e4e9a9113693c098bb743', 'MangaFenix', 'https://manga-fenix.com')
 	AddWebsiteModule('a8ad2a81768742caa7135047f28c2b00', 'SwordManga', 'https://swordmanga.com')
 	AddWebsiteModule('rrad2a81768742caa7135047f282b777', 'MangaMonarca', 'https://mangamonarca.xyz')
---
+++ lua/modules/RagnarokScan.lua
@@ -0,0 +1,55 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '36b0f19e144a4642a2839ec9cb728c3a'
+	m.Name                     = 'RagnarokScan'
+	m.RootURL                  = 'https://ragnarokscanlation.net'
+	m.Category                 = 'Spanish-Scanlation'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.Madara'
+-- XPathTokenAuthors = 'Author(s)'
+-- XPathTokenArtists = 'Artist(s)'
+-- XPathTokenGenres  = 'Genre(s)'
+-- XPathTokenStatus  = 'Status'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	MANGAINFO.Summary   = CreateTXQuery(HTTP.Document).XPathString('//div[@class="manga-excerpt"]')
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return no_error
+end
