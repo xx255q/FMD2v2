fb4b53da7a411c2cf8048c195e9fb332e958d202
Slasar41 <agungtrianto41@outlook.com>
2024-08-11 22:43:01 +0700

    TMOHentai: Fixed all (#1546)

--- lua/modules/TMOHentai.lua
+++ lua/modules/TMOHentai.lua
@@ -1,74 +1,90 @@
-function getinfo()
-	MANGAINFO.URL=MaybeFillHost(MODULE.RootURL, URL)
-	if HTTP.GET(MANGAINFO.URL) then
-		x=CreateTXQuery(HTTP.Document)
-		MANGAINFO.Title=x.XPathString('//h3')
-		MANGAINFO.CoverLink=x.XPathString('//*[@alt="cover"]/@src')
-		MANGAINFO.Artists=x.XPathStringAll('//*[@class="tag tag-accepted"][contains(@href,"=artist")]/text()')
-		MANGAINFO.Genres=x.XPathStringAll('//*[@class="list-inline"][2]/a/text()')
-		MANGAINFO.ChapterLinks.Add(URL)
-		MANGAINFO.ChapterNames.Add(MANGAINFO.Title)
-		return no_error
-	else
-		return net_problem
-	end
-end
-
-function getpagenumber()
-	TASK.PageLinks.Clear()
-	TASK.PageNumber=0
-	URL = MODULE.RootURL..URL..'/paginated/1'
-	URL = string.gsub(URL, 'contents', 'reader')
-	if HTTP.GET(URL) then
-		TASK.PageNumber=CreateTXQuery(HTTP.Document).XPathCount('//select[@id="select-page"]/option')
-	else
-		return false
-	end
-	return true
-end
-
-function getimageurl()
-	URL = MODULE.RootURL..URL..'/paginated/'
-	URL = string.gsub(URL, 'contents', 'reader')
-	if HTTP.GET(URL..(WORKID+1)) then
-		local img = CreateTXQuery(HTTP.Document).XPathString('//*[@class="content-image lazy"]/@data-original')
-		TASK.PageLinks[WORKID]=MODULE.RootURL..img
-		return true
-	end
-	return false
-end
-
-function getnameandlink()
-	local s = '/section/all?page='..(URL + 1)
-	if HTTP.GET(MODULE.RootURL .. s) then
-		local x = CreateTXQuery(HTTP.Document)
-		local v = x.XPath('//*[@class="content-title truncate"]/a')
-		local hasTitles = false
-		for i = 1, v.Count do
-			local v1 = v.Get(i)
-			LINKS.Add(v1.GetAttribute('href'))
-			NAMES.Add(v1.GetAttribute('title'))
-			hasTitles = true
-		end
-		if hasTitles then
-			UPDATELIST.CurrentDirectoryPageNumber = UPDATELIST.CurrentDirectoryPageNumber + 1
-		end
-		return no_error
-	else
-		return net_problem
-	end
-end
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
 
 function Init()
 	local m = NewWebsiteModule()
-	m.ID = '499c85a7b2e542609cb564a196dffb40'
-	m.Category='H-Sites'
-	m.Name='TMOHentai'
-	m.RootURL='https://www.tmohentai.com'
-	m.LastUpdated='May 20, 2019'
-	m.OnGetInfo='getinfo'
-	m.OnGetPageNumber='getpagenumber'
-	m.OnGetImageURL='getimageurl'
-	m.OnGetNameAndLink='getnameandlink'
-	m.SortedList=true
+	m.ID                       = '499c85a7b2e542609cb564a196dffb40'
+	m.Name                     = 'TMOHentai'
+	m.RootURL                  = 'https://tmohentai.com'
+	m.Category                 = 'H-Sites'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+	m.OnBeforeDownloadImage    = 'BeforeDownloadImage'
+	m.SortedList               = true
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+DirectoryPagination = '/section/all?view=list&order=publication_date&order-dir=desc&type=all&page=1'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	local next_url, x = nil
+	local u = MODULE.RootURL .. DirectoryPagination
+
+	if not HTTP.GET(u) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	while true do
+		x.XPathHREFAll('//td[@class="text-left"]/a', LINKS, NAMES)
+		next_url = x.XPathString('//ul[@class="pagination"]/li[last()]/a/@href')
+		if HTTP.Terminated then break end
+		if next_url == '' then break end
+		UPDATELIST.UpdateStatusText('Loading page ' .. (next_url:match('page=(%d+)') or ''))
+		if HTTP.GET(next_url) then
+			x.ParseHTML(HTTP.Document)
+		else
+			break
+		end
+	end
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	local v, x = nil
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	MANGAINFO.Title     = x.XPathString('//div[@class="panel-heading"]/h3')
+	MANGAINFO.CoverLink = x.XPathString('//img[@alt="cover"]/@src')
+	MANGAINFO.Artists   = x.XPathStringAll('//div[@class="content-property"]//a[contains(@href, "=artist")]')
+	MANGAINFO.Genres    = x.XPathStringAll('//div[@class="content-property"]//a[contains(@href, "genders") or contains(@href, "tag")]')
+
+	MANGAINFO.ChapterLinks.Add(MANGAINFO.URL)
+	MANGAINFO.ChapterNames.Add(MANGAINFO.Title)
+
+	HTTP.Reset()
+	HTTP.Headers.Values['Referer'] = MANGAINFO.URL
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	local u = MaybeFillHost(MODULE.RootURL, URL):gsub('contents', 'reader') .. '/cascade'
+
+	if not HTTP.GET(u) then return net_problem end
+
+	CreateTXQuery(HTTP.Document).XPathStringAll('//img[@class="content-image lazy"]/@data-original', TASK.PageLinks)
+
+	return no_error
+end
+
+-- Prepare the URL, http header and/or http cookies before downloading an image.
+function BeforeDownloadImage()
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL
+
+	return true
 end
