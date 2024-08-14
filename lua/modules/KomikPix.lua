401352cc12237c61f5911d0c64bcbfe4a750482b
Slasar41 <agungtrianto41@outlook.com>
2024-08-14 21:32:38 +0700

    KomikPix: Added website support (#1550)

---
+++ lua/modules/KomikPix.lua
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = 'ed137c7584054880951c49d1be3f6863'
+	m.Name                     = 'KomikPix'
+	m.RootURL                  = 'https://komikpix.com/'
+	m.Category                 = 'H-Sites'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.MangaThemesia'
+DirectoryPagination = '/hentai/list-mode/'
+-- XPathTokenAuthors   = 'Author'
+-- XPathTokenArtists   = 'Artist'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return no_error
+end
