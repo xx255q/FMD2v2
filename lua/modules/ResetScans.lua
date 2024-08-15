c221b9341bf929d02f57dd55782b327879e20463
Slasar41 <agungtrianto41@outlook.com>
2024-08-15 11:17:08 +0700

    ResetScans: Changed domain (#1551)

---
+++ lua/modules/ResetScans.lua
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '287f665620664e468d4e05f5d76f5a43'
+	m.Name                     = 'ResetScans'
+	m.RootURL                  = 'https://reset-scans.co'
+	m.Category                 = 'English-Scanlation'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.Madara'
+-- XPathTokenAuthors = 'Author(s)'
+-- XPathTokenArtists = 'Artist(s)'
+-- XPathTokenGenres  = 'Genre(s)'
+-- XPathTokenStatus  = 'Status'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return no_error
+end
