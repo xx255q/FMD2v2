c0c1ff44b9aaea2972d12f3ed092b85784a2aad6
Slasar41 <agungtrianto41@outlook.com>
2024-08-15 21:34:10 +0700

    ToonGod: Added website support (#1555)

---
+++ lua/modules/ToonGod.lua
@@ -0,0 +1,57 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '24cd8bdc37ca4dd68c81826c68a0091f'
+	m.Name                     = 'ToonGod'
+	m.RootURL                  = 'https://www.toongod.org'
+	m.Category                 = 'Webcomics'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+	m.OnBeforeDownloadImage    = 'BeforeDownloadImage'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.Madara'
+-- XPathTokenAuthors = 'Author(s)'
+-- XPathTokenArtists = 'Artist(s)'
+-- XPathTokenGenres  = 'Genre(s)'
+-- XPathTokenStatus  = 'Status'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return no_error
+end
+
+-- Prepare the URL, http header and/or http cookies before downloading an image.
+function BeforeDownloadImage()
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL
+
+	return true
+end
