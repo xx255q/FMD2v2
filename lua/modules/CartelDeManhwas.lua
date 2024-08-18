4c028895c6767bc2c2e82b2a12f9a542b61af3d2
Slasar41 <agungtrianto41@outlook.com>
2024-08-18 20:26:27 +0700

    CartelDeManhwas: Added website support (#1579)

---
+++ lua/modules/CartelDeManhwas.lua
@@ -0,0 +1,50 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '9a0a1298ca044dd9886a3446092f285e'
+	m.Name                     = 'CartelDeManhwas'
+	m.RootURL                  = 'https://carteldemanhwas.com'
+	m.Category                 = 'Spanish'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.MangaThemesia'
+DirectoryPagination = '/series/list-mode/'
+-- XPathTokenAuthors   = 'Author'
+-- XPathTokenArtists   = 'Artist'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return no_error
+end
