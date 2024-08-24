547bf58e5aecbac8a4ca021be8d80e61f6b6c297
Slasar41 <agungtrianto41@outlook.com>
2024-08-24 22:37:09 +0700

    RavenSeries: Fixed all (#1596)
    * RavenSeries: Added website support
    
    * Forgot there's already the same module

--- lua/modules/Ravenmanga.lua
+++ lua/modules/Ravenmanga.lua
@@ -5,9 +5,9 @@
 function Init()
 	local m = NewWebsiteModule()
 	m.ID                       = '3a3f61e584334c26923cfcb160a5f50b'
-	m.Name                     = 'Ravenmanga'
-	m.RootURL                  = 'https://ravenmanga.xyz'
-	m.Category                 = 'Spanish'
+	m.Name                     = 'RavenSeries'
+	m.RootURL                  = 'https://ravensword.lat'
+	m.Category                 = 'Spanish-Scanlation'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
 	m.OnGetPageNumber          = 'GetPageNumber'
@@ -17,7 +17,8 @@ end
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-DirectoryPagination = '/comic'
+local Template = require 'templates.MangaEsp'
+DirectoryPagination = '/comics'
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
@@ -25,36 +26,38 @@ DirectoryPagination = '/comic'
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
-	local dirurl = MODULE.RootURL .. DirectoryPagination
-	if not HTTP.GET(dirurl) then return net_problem end
-	local x = CreateTXQuery(HTTP.Document)
-	local next_url
-	while true do
-		x.XPathHREFAll('//a[contains(@class, "link_title")]', LINKS, NAMES)
-		next_url = x.XPathString('//ul[@class="pagination"]/li[last()]/a/@href')
-		if HTTP.Terminated then break end
-		if next_url == '' then break end
-		UPDATELIST.UpdateStatusText('Loading page ' .. (next_url:match('page=(%d+)') or ''))
-		if HTTP.GET(next_url) then
-			x.ParseHTML(HTTP.Document)
-		else
-			break
-		end
+	local v, x = nil
+	local u = MODULE.RootURL .. DirectoryPagination
+
+	if not HTTP.GET(u) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	x.ParseHTML(GetBetween('proyectos = ', '}];', x.XPathString('//script[contains(., "proyectos")]')) .. '}]')
+	for v in x.XPath('json(*)()').Get() do
+		LINKS.Add('sr2/' .. v.GetProperty('slug').ToString())
+		NAMES.Add(v.GetProperty('nombre').ToString())
 	end
+
 	return no_error
 end
 
 -- Get info and chapter list for current manga.
 function GetInfo()
-	if not HTTP.GET(MaybeFillHost(MODULE.RootURL, URL)) then return net_problem end
+	local v, x = nil
+	local u = MaybeFillHost(MODULE.RootURL, URL)
 
-	local x = CreateTXQuery(HTTP.Document)
-	MANGAINFO.Title     = x.XPathString('//div/span[@class="info-span"]')
-	MANGAINFO.CoverLink = x.XPathString('//img[@class="image_manga"]/@src')
-	MANGAINFO.Genres    = x.XPathStringAll('//div[contains(., "Generos")]/following-sibling::div/span')
-	MANGAINFO.Summary   = x.XPathString('//div[contains(@class, "text-muted")]')
+	if not HTTP.GET(u) then return net_problem end
 
-	x.XPathHREFAll('//a[contains(@class, "cap-link")]', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
+	x = CreateTXQuery(HTTP.Document)
+	MANGAINFO.Title     = x.XPathString('//h1')
+	MANGAINFO.CoverLink = x.XPathString('//div[contains(@class, "max-w-80 w-full")]/img/@src')
+	MANGAINFO.Genres    = x.XPathStringAll('//div[@class="flex flex-wrap"]/a')
+	MANGAINFO.Summary   = x.XPathString('//p[@class="font-light text-gray-400"]')
+
+	for v in x.XPath('//div[@class="grid md:grid-cols-2 gap-4"]/a').Get() do
+		MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
+		MANGAINFO.ChapterNames.Add(x.XPathString('div[2]/div/div[@id="name"]', v))
+	end
 	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
 	return no_error
@@ -62,9 +65,7 @@ end
 
 -- Get the page count for the current chapter.
 function GetPageNumber()
-	if not HTTP.GET(MaybeFillHost(MODULE.RootURL, URL)) then return net_problem end
-
-	CreateTXQuery(HTTP.Document).XPathStringAll('//div[contains(@class, "container-image")]//img/@src', TASK.PageLinks)
+	Template.GetPageNumber()
 
 	return no_error
 end
