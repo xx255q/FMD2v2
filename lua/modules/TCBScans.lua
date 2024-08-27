00d195d43a879714a1279a15e2edbcd759775f19
Slasar41 <agungtrianto41@outlook.com>
2024-08-27 20:46:36 +0700

    TCBScans: Added website support (#1599)

---
+++ lua/modules/TCBScans.lua
@@ -0,0 +1,70 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '80cb4513684e4b4ebc68730d0813781c'
+	m.Name                     = 'TCBScans'
+	m.RootURL                  = 'https://tcbscans.me'
+	m.Category                 = 'English-Scanlation'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+DirectoryPagination = '/projects'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	local u = MODULE.RootURL .. DirectoryPagination
+
+	if not HTTP.GET(u) then return net_problem end
+
+	CreateTXQuery(HTTP.Document).XPathHREFAll('//div[@class="flex flex-col"]/a', LINKS, NAMES)
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	local v, x = nil
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	MANGAINFO.Title     = x.XPathString('//h1')
+	MANGAINFO.CoverLink = x.XPathString('//div[@class="flex items-center justify-center"]/img/@src')
+	MANGAINFO.Summary   = x.XPathString('//p[@class="leading-6 my-3"]')
+
+	for v in x.XPath('//div[@class="col-span-2"]/a').Get() do
+		title = x.XPathString('div[2]', v)
+		title = title ~= 'null' and title ~= '' and string.format(' - %s', title) or ''
+
+		MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
+		MANGAINFO.ChapterNames.Add(x.XPathString('div[1]', v) .. title)
+	end
+	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	CreateTXQuery(HTTP.Document).XPathStringAll('//img[@class="fixed-ratio-content"]/@src', TASK.PageLinks)
+
+	return no_error
+end
