bcff7f3058504e9faec3a1c50dcedf405f393b8e
Slasar41 <agungtrianto41@outlook.com>
2024-08-30 15:09:02 +0700

    FlameComics: Changed domain (#1610)

--- lua/modules/FlameComics.lua
+++ lua/modules/FlameComics.lua
@@ -6,7 +6,7 @@ function Init()
 	local m = NewWebsiteModule()
 	m.ID                       = 'fb34a56c83f54b19b57a9a92070fe899'
 	m.Name                     = 'FlameComics'
-	m.RootURL                  = 'https://flamecomics.com'
+	m.RootURL                  = 'https://flamecomics.me'
 	m.Category                 = 'English-Scanlation'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
@@ -37,10 +37,12 @@ end
 function GetInfo()
 	Template.GetInfo()
 
+	local v, x = nil
+
 	x = CreateTXQuery(HTTP.Document)
-	for v in x.XPath('//*[@id="chapterlist"]//li/a').Get() do
+	for v in x.XPath('//div[@id="chapterlist"]//li/a').Get() do
 		MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
-		MANGAINFO.ChapterNames.Add(x.XPathString('div/div/span[@class="chapternum"]',v))
+		MANGAINFO.ChapterNames.Add(x.XPathString('div/div/span[@class="chapternum"]/normalize-space(.)', v))
 	end
 	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
@@ -49,7 +51,19 @@ end
 
 -- Get the page count for the current chapter.
 function GetPageNumber()
-	Template.GetPageNumber()
+	local v, x = nil
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	if not HTTP.GET(u) then return net_problem end
+	
+	x = CreateTXQuery(HTTP.Document)
+	x.ParseHTML(GetBetween('run(', ');', x.XPathString('//script[contains(., "ts_reader")]')))
+	for v in x.XPath('json(*).sources()[1].images()').Get() do
+		if string.find(v.ToString(), 'radio', 1, true) == nil and
+		string.find(v.ToString(), 'credit', 1, true) == nil then
+			TASK.PageLinks.Add(v.ToString())
+		end
+	end
 
 	return no_error
 end
