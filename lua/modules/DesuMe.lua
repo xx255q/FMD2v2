89609d3a84b9f27f9b5dcc651d4834496b0dbba7
Slasar41 <agungtrianto41@outlook.com>
2024-09-01 11:15:55 +0700

    DesuMe: Refactor, use API (#1611)

--- lua/modules/DesuMe.lua
+++ lua/modules/DesuMe.lua
@@ -1,70 +1,124 @@
-function getinfo()
-	MANGAINFO.URL=MaybeFillHost(MODULE.RootURL, URL)
-	if HTTP.GET(MANGAINFO.URL) then
-		local x=CreateTXQuery(HTTP.Document)
-		MANGAINFO.Title=x.XPathString('//h1/span[@class="name"]')
-		MANGAINFO.CoverLink=MaybeFillHost(MODULE.RootURL, x.XPathString('//img[@itemprop="image"]/@src'))
-		MANGAINFO.Genres=x.XPathStringAll('//ul[@class="tagList"]/li/a')
-		MANGAINFO.Status = MangaInfoStatusIfPos(x.XPathString('//span[contains(@class, "status_tag")]'), 'выходит', 'издано')
-		MANGAINFO.Summary=x.XPathString('//div[@itemprop="description"]')
-		x.XPathHREFAll('//ul[@class="chlist"]/li/h4/a', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
-		MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
-		return no_error
-	else
-		return net_problem
-	end
-end
-
-function getpagenumber()
-	TASK.PageLinks.Clear()
-	TASK.PageNumber=0
-	if HTTP.GET(MaybeFillHost(MODULE.RootURL, URL)) then
-		local x=CreateTXQuery(HTTP.Document)
-		local s = x.XPathString('//script[contains(., "Reader.init")]')
-		s = GetBetween('.init(', ');', s)
-		x.ParseHTML(s)
-		local dir = x.XPathString('json(*).dir')
-		local v = x.XPath('json(*).images()()')
-		for i = 1,v.Count,3 do
-			local v1=v.Get(i)
-			TASK.PageLinks.Add(dir .. v1.ToString())
-		end
-	else
-		return false
-	end
-	return true
-end
-
-local dirurl = '/manga/?order_by=name&page='
-function getnameandlink()
-	if HTTP.GET(MODULE.RootURL .. dirurl .. (URL + 1)) then
-		local x = CreateTXQuery(HTTP.Document)
-		x.XPathHREFAll('//div[@class="animeList"]/ol/li/div/h3/a', LINKS, NAMES)
-		return no_error
-	else
-		return net_problem
-	end
-end
-
-function getdirectorypagenumber()
-	if HTTP.GET(MODULE.RootURL .. dirurl .. '1') then
-		x = CreateTXQuery(HTTP.Document)
-		PAGENUMBER = tonumber(x.XPathString('//div[@class="PageNav"]/nav/a[last()-1]')) or 1
-		return no_error
-	else
-		return net_problem
-	end
-end
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
 
 function Init()
 	local m = NewWebsiteModule()
-	m.ID = '41e78386ff3447e7a283b6ce55950f0f'
-	m.Name = 'DesuMe'
-	m.RootURL = 'https://desu.me'
-	m.Category = 'Russian'
-	m.LastUpdated='March 3, 2018'
-	m.OnGetInfo='getinfo'
-	m.OnGetPageNumber='getpagenumber'
-	m.OnGetNameAndLink='getnameandlink'
-	m.OnGetDirectoryPageNumber = 'getdirectorypagenumber'
+	m.ID                       = '41e78386ff3447e7a283b6ce55950f0f'
+	m.Name                     = 'DesuMe'
+	m.RootURL                  = 'https://desu.me'
+	m.Category                 = 'Russian'
+	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+	m.OnBeforeDownloadImage    = 'BeforeDownloadImage'
+	m.MaxTaskLimit             = 2
+	m.MaxConnectionLimit       = 3
+	m.SortedList               = true
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+API_URL = '/manga/api/'
+DirectoryPagination = '?limit=50&order=id&page='
+USER_AGENT = 'FreeMangaDownloader'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get the page count of the manga list of the current website.
+function GetDirectoryPageNumber()
+	local x = nil
+	local u = MODULE.RootURL .. API_URL .. DirectoryPagination .. 1
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL
+	HTTP.UserAgent = USER_AGENT
+
+	if not HTTP.GET(u) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	PAGENUMBER = tonumber(math.floor(x.XPathString('json(*).pageNavParams.count') / x.XPathString('json(*).pageNavParams.limit') + 0.5)) or 1
+
+	return no_error
+end
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	local u = MODULE.RootURL .. API_URL .. DirectoryPagination .. (URL + 1)
+	sleep(1000)
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL
+	HTTP.UserAgent = USER_AGENT
+
+	if not HTTP.GET(u) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	for v in x.XPath('json(*).response()').Get() do
+		LINKS.Add(v.GetProperty('url').ToString())
+		NAMES.Add(v.GetProperty('russian').ToString())
+	end
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	local chapter, json, title, v, volume, x = nil
+	local u = MODULE.RootURL .. API_URL .. URL:match('.(%d+)/$')
+	sleep(1000)
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL
+	HTTP.UserAgent = USER_AGENT
+
+	if not HTTP.GET(u) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	json = x.XPath('json(*).response')
+	MANGAINFO.Title     = x.XPathString('russian', json)
+	MANGAINFO.CoverLink = x.XPathString('image/preview', json)
+	MANGAINFO.Genres    = x.XPathStringAll('json(*).response.genres().russian')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('trans_status', json), 'continued', 'completed')
+	MANGAINFO.Summary   = x.XPathString('description', json)
+
+	for v in x.XPath('json(*).response.chapters.list()').Get() do
+		volume = v.GetProperty('vol').ToString()
+		chapter = v.GetProperty('ch').ToString()
+		title = v.GetProperty('title').ToString()
+
+		volume = volume ~= 'null' and string.format('Том %s. ', volume) or ''
+		chapter = chapter ~= 'null' and string.format('Глава %s', chapter) or ''
+		title = title ~= 'null' and title ~= '' and string.format(' - %s', title) or ''
+
+		MANGAINFO.ChapterLinks.Add(x.XPathString('id', json) .. '/chapter/' .. v.GetProperty('id').ToString())
+		MANGAINFO.ChapterNames.Add(volume .. chapter .. title)
+	end
+	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	local v = nil
+	local u = MODULE.RootURL .. API_URL .. URL:gsub('^/', '')
+	sleep(1000)
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL
+	HTTP.UserAgent = USER_AGENT
+
+	if not HTTP.GET(u) then return net_problem end
+
+	for v in CreateTXQuery(HTTP.Document).XPath('json(*).response.pages.list()').Get() do
+		TASK.PageLinks.Add(v.GetProperty('img').ToString())
+	end
+
+	return no_error
+end
+
+-- Prepare the URL, http header and/or http cookies before downloading an image.
+function BeforeDownloadImage()
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL
+	HTTP.UserAgent = USER_AGENT
+
+	return true
 end
