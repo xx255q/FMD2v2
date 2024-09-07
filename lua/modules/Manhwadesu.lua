32b7a49d37929974b9da32b809e24cb590e43fc1
Slasar41 <agungtrianto41@outlook.com>
2024-09-07 20:40:40 +0700

    Manhwadesu: Added website support (#1639)

---
+++ lua/modules/Manhwadesu.lua
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = 'ee60f64cc450424bb906af3f699fd7e4'
+	m.Name                     = 'Manhwadesu'
+	m.RootURL                  = 'https://manhwadesu.cc'
+	m.Category                 = 'H-Sites'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.MangaThemesia'
+DirectoryPagination = '/komik/list-mode/'
+-- XPathTokenAuthors   = 'Author'
+-- XPathTokenArtists   = 'Artist'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return no_error
+end
