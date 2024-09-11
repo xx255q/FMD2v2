1cc6546c5889c82ce955220b67bbed858c1035cf
Slasar41 <agungtrianto41@outlook.com>
2024-09-11 11:50:48 +0700

    MaidMangaID: Changed template (#1652)

--- lua/modules/MaidMangaID.lua
+++ lua/modules/MaidMangaID.lua
@@ -17,10 +17,8 @@ end
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-local Template = require 'templates.MangaThemesia'
-DirectoryPagination = '/manga-list/'
--- XPathTokenAuthors   = 'Author'
--- XPathTokenArtists   = 'Artist'
+local Template = require 'templates.ZManga'
+-- DirectoryPagination = '/manga-list/'
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
@@ -30,8 +28,6 @@ DirectoryPagination = '/manga-list/'
 function GetNameAndLink()
 	Template.GetNameAndLink()
 
-	CreateTXQuery(HTTP.Document).XPathHREFAll('//div[@class="mangalist-blc"]//a', LINKS, NAMES)
-
 	return no_error
 end
 
@@ -39,28 +35,12 @@ end
 function GetInfo()
 	Template.GetInfo()
 
-	x = CreateTXQuery(HTTP.Document)
-	MANGAINFO.Title     = x.XPathString('//div[@class="series-title"]')
-	MANGAINFO.CoverLink = x.XPathString('//div[@class="series-thumb"]/img/@src')
-	MANGAINFO.Authors   = x.XPathString('//li[contains(b, "Author")]/span')
-	MANGAINFO.Genres    = x.XPathStringAll('//div[@class="series-genres"]/a')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//span[contains(@class, "status")]'))
-	MANGAINFO.Summary   = x.XPathString('//div[@class="series-synops"]/string-join(.//text(), " ")')
-
-	for v in x.XPath('//ul[@class="series-chapterlist"]//a').Get() do
-		MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
-		MANGAINFO.ChapterNames.Add(x.XPathString('span[1]/text()', v))
-	end		
-	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
-
 	return no_error
 end
 
 -- Get the page count for the current chapter.
 function GetPageNumber()
 	Template.GetPageNumber()
-
-	CreateTXQuery(HTTP.Document).XPathStringAll('//div[@class="reader-area"]//img/@src', TASK.PageLinks)
 
 	return no_error
 end
