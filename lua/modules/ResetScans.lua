3afcb046135309190a28a0ab59fdd061dc5f1c4d
Slasar41 <agungtrianto41@outlook.com>
2024-10-04 13:32:04 +0700

    ResetScans: Changed domain (#1697)

--- lua/modules/ResetScans.lua
+++ lua/modules/ResetScans.lua
@@ -6,7 +6,7 @@ function Init()
 	local m = NewWebsiteModule()
 	m.ID                       = '287f665620664e468d4e05f5d76f5a43'
 	m.Name                     = 'ResetScans'
-	m.RootURL                  = 'https://reset-scans.co'
+	m.RootURL                  = 'https://rspro.xyz'
 	m.Category                 = 'English-Scanlation'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
