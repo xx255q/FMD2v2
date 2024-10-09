2111f1822e1bffc76a8df480893d595ff2a73a95
Slasar41 <agungtrianto41@outlook.com>
2024-10-09 11:43:01 +0700

    KappaBeast: Fixed manga info (#1708)

--- lua/modules/KappaBeast.lua
+++ lua/modules/KappaBeast.lua
@@ -18,7 +18,7 @@ end
 ----------------------------------------------------------------------------------------------------
 
 local Template = require 'templates.MangaThemesia'
-DirectoryPagination = '/series/list-mode/'
+-- DirectoryPagination = '/manga/list-mode/'
 -- XPathTokenAuthors   = 'Author'
 -- XPathTokenArtists   = 'Artist'
 
@@ -37,28 +37,12 @@ end
 function GetInfo()
 	Template.GetInfo()
 
-	local v, x = nil
-
-	x = CreateTXQuery(HTTP.Document)
-	MANGAINFO.Authors   = x.XPathString('//span[contains(., "' .. XPathTokenAuthors .. '")]/following-sibling::span')
-	MANGAINFO.Artists   = x.XPathString('//span[contains(., "' .. XPathTokenArtists .. '")]/following-sibling::span')
-	MANGAINFO.Genres    = x.XPathStringAll('//div[@class="sertogenre"]/a')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//div[@class="sertostat"]/span'))
-
-	for v in x.XPath('//div[@class="eplister eplisterfull"]/ul//a').Get() do
-		MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
-		MANGAINFO.ChapterNames.Add(x.XPathString('div[@class="epl-num"]/normalize-space(.)', v))
-	end
-	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
-
 	return no_error
 end
 
 -- Get the page count for the current chapter.
 function GetPageNumber()
 	Template.GetPageNumber()
-
-	CreateTXQuery(HTTP.Document).XPathStringAll('//div[@class="epcontent entry-content"]/p/img/@src', TASK.PageLinks)
 
 	return no_error
 end
