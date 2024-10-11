7bfcf8ed27340bee6422e16aa947dba67e2d06d2
Slasar41 <agungtrianto41@outlook.com>
2024-10-11 14:55:50 +0700

    WeebCentral: Fixed manga list (#1723)

--- lua/modules/WeebCentral.lua
+++ lua/modules/WeebCentral.lua
@@ -19,8 +19,8 @@ end
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-DirectoryPagination = '/search?limit=24&sort=Recently+Added&order=Descending&official=Any&offset='
-DirectoryPageLimit = 24
+DirectoryPagination = '/search/data?limit=32&sort=Recently+Added&order=Descending&official=Any&display_mode=Full+Display&offset='
+DirectoryPageLimit = 32
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
@@ -30,17 +30,17 @@ DirectoryPageLimit = 24
 function GetDirectoryPageNumber()
 	local s, x = nil
 	local u = MODULE.RootURL .. DirectoryPagination .. 0
-	PAGENUMBER = 8000
+	PAGENUMBER = 8300
 
 	if not HTTP.GET(u .. PAGENUMBER) then return net_problem end
 
 	x = CreateTXQuery(HTTP.Document)
-	s = x.XPathString('//article/button/@hx-get')
+	s = x.XPathString('//button/@hx-get')
 	while string.len(s) > 0 do
 		PAGENUMBER = tonumber(s:match('offset=(%d+)')) or PAGENUMBER + DirectoryPageLimit
 		if not HTTP.GET(u .. PAGENUMBER) then return net_problem end
 		x = CreateTXQuery(HTTP.Document)
-		s = x.XPathString('//article/button/@hx-get')
+		s = x.XPathString('//button/@hx-get')
 	end
 	PAGENUMBER = math.ceil(PAGENUMBER / DirectoryPageLimit)
 
@@ -49,12 +49,17 @@ end
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
+	local v, x = nil
 	local u = MODULE.RootURL
 	if URL ~= '0' then u = u .. DirectoryPagination .. (DirectoryPageLimit * tonumber(URL)) end
 
 	if not HTTP.GET(u) then return net_problem end
 
-	CreateTXQuery(HTTP.Document).XPathHREFAll('//article/section/div/a', LINKS, NAMES)
+	x = CreateTXQuery(HTTP.Document)
+	for v in x.XPath('//article/section/a').Get() do
+		LINKS.Add(v.GetAttribute('href'))
+		NAMES.Add(x.XPathString('article[2]/div[2]/div[2]/div', v))
+	end
 
 	return no_error
 end
