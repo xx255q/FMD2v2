2ae810f0b1c0841c5a1372b724c4b195ec3a9d74
Slasar41 <agungtrianto41@outlook.com>
2024-10-26 22:03:20 +0700

    Madara: Added another decryption method (#1769)

--- lua/templates/Madara.lua
+++ lua/templates/Madara.lua
@@ -36,7 +36,7 @@ function _M.GetNameAndLink()
 	return no_error
 end
 
--- Get info and chapter list for current manga.
+-- Get info and chapter list for the current manga.
 function _M.GetInfo()
 	local id, s, x = nil
 	local u = MaybeFillHost(MODULE.RootURL, URL)
@@ -87,7 +87,7 @@ end
 
 -- Get the page count for the current chapter.
 function _M.GetPageNumber()
-	local i, x = nil
+	local i, img, script, x = nil
 	local u = MaybeFillHost(MODULE.RootURL, URL)
 	if string.find(u, 'style=list', 1, true) == nil then u = string.gsub(u, '?style=paged', '') .. '?style=list' end
 
@@ -96,6 +96,19 @@ function _M.GetPageNumber()
 	x = CreateTXQuery(HTTP.Document)
 	x.XPathStringAll('//div[contains(@class, "page-break")]/img/@data-src', TASK.PageLinks)
 	if TASK.PageLinks.Count == 0 then x.XPathStringAll('//div[contains(@class, "page-break")]/img/@src', TASK.PageLinks) end
+	if TASK.PageLinks.Count == 0 then
+		script = x.XPathString('//script[@id="chapter-protector-data"]')
+		img = require("fmd.duktape").ExecJS(script .. [[
+
+		var CryptoJS = require("utils/crypto-js.min.js");
+		var CryptoJSAesJson = require("utils/cryptojs-aes-format.js");
+		JSON.parse(CryptoJS.AES.decrypt(chapter_data, wpmangaprotectornonce, {format: CryptoJSAesJson}).toString(CryptoJS.enc.Utf8));
+
+		]]):gsub('\\/', '/'):gsub('%[', ''):gsub('%]', '')
+		for i in img:gmatch('"([^",]+)') do
+			TASK.PageLinks.Add(i)
+		end
+	end
 	if TASK.PageLinks.Count == 0 then
 		x.ParseHTML('[' .. GetBetween('[', ']', x.XPathString('//script[@id="chapter_preloaded_images"]')) .. ']')
 		x.XPathStringAll('json(*)().src', TASK.PageLinks)
