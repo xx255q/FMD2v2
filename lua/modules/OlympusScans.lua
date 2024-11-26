5e75ed3a30e0a2b5deeddaff6c5fb47e739be055
Slasar41 <agungtrianto41@outlook.com>
2024-11-26 21:53:22 +0700

    OlympusScans: Changed domain (#1852)

--- lua/modules/OlympusScans.lua
+++ lua/modules/OlympusScans.lua
@@ -6,7 +6,7 @@ function Init()
 	local m = NewWebsiteModule()
 	m.ID                       = '760d177b1f6d4763a08971c0c1b5572b'
 	m.Name                     = 'Olympus Scanlation'
-	m.RootURL                  = 'https://zonaolympus.com'
+	m.RootURL                  = 'https://olympuscomic.com'
 	m.Category                 = 'Spanish-Scanlation'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
@@ -17,7 +17,7 @@ end
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-API_URL = 'https://dashboard.zonaolympus.com/api'
+API_URL = 'https://dashboard.olympuscomic.com/api'
 DirectoryPagination = '/series?page='
 
 ----------------------------------------------------------------------------------------------------
