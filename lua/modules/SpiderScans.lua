1d353903ae0ae9138b38f01feb7dd7c237616da9
Slasar41 <agungtrianto41@outlook.com>
2024-12-10 15:45:24 +0700

    SpiderScans: Added website support (#1878)

---
+++ lua/modules/SpiderScans.lua
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = 'f0c566f6b9cd470fa580a5f238e1fda6'
+	m.Name                     = 'SpiderScans'
+	m.RootURL                  = 'https://spiderscans.xyz'
+	m.Category                 = 'English-Scanlation'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.MangaThemesia'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	CreateTXQuery(HTTP.Document).XPathStringAll('//div[@id="readerarea"]//img/@src', TASK.PageLinks)
+
+	return no_error
+end
