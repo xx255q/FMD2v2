66290bd129fcb75805825aba64fd7591d05b4efd
Slasar41 <agungtrianto41@outlook.com>
2024-12-15 14:49:15 +0700

    Hive Scans: Changed template and an option to show paid chapters (#1894)
    * Hive Scans: Changed template and an option to show paid chapters
    
    * Update SUPPORTED_WEBSITES.md

--- lua/modules/VoidScans.lua
+++ lua/modules/VoidScans.lua
@@ -5,26 +5,50 @@
 function Init()
 	local m = NewWebsiteModule()
 	m.ID                       = 'f01040ee781d4ae1929031419b97d2e0'
-	m.Name                     = 'VoidScans'
-	m.RootURL                  = 'https://void-scans.com'
+	m.Name                     = 'Hive Scans'
+	m.RootURL                  = 'https://hivetoon.com'
 	m.Category                 = 'English-Scanlation'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
 	m.OnGetPageNumber          = 'GetPageNumber'
+	m.OnLogin                  = 'Login'
+	m.AccountSupport           = true
+
+	local fmd = require 'fmd.env'
+	local slang = fmd.SelectedLanguage
+	local lang = {
+		['en'] = {
+			['showpaidchapters'] = 'Show paid chapters'
+		},
+		['id_ID'] = {
+			['showpaidchapters'] = 'Tampilkan bab berbayar'
+		},
+		get =
+			function(self, key)
+				local sel = self[slang]
+				if sel == nil then sel = self['en'] end
+				return sel[key]
+			end
+	}
+	m.AddOptionCheckBox('showpaidchapters', lang:get('showpaidchapters'), false)
 end
 
 ----------------------------------------------------------------------------------------------------
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-local Template = require 'templates.MangaThemesia'
--- DirectoryPagination = '/manga/list-mode/'
--- XPathTokenAuthors   = 'Author'
--- XPathTokenArtists   = 'Artist'
+local Template = require 'templates.Iken'
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
 ----------------------------------------------------------------------------------------------------
+
+-- Login account to the current website.
+function Login()
+	Template.Login()
+
+	return no_error
+end
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
