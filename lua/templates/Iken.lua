7374734c79f82ef1177fd483d7cf32d25dcabd61
Slasar41 <agungtrianto41@outlook.com>
2024-12-15 14:32:41 +0700

    Iken: Added account support and hide paid chapters by default (#1893)

--- lua/templates/Iken.lua
+++ lua/templates/Iken.lua
@@ -14,6 +14,28 @@ DirectoryPagination = '/api/query?perPage=9999'
 -- Event Functions
 ----------------------------------------------------------------------------------------------------
 
+-- Login account to the current website.
+function _M.Login()
+	local s, x = nil
+	local login_url = MODULE.RootURL .. '/auth/signin'
+	if MODULE.Account.Enabled == false then return false end
+	s = '[{"email":"' .. MODULE.Account.Username ..
+	'","password":"' .. MODULE.Account.Password .. '"}]'
+	MODULE.Account.Status = asChecking
+	if HTTP.POST(login_url, s) then
+		if (HTTP.ResultCode == 200) and (HTTP.Cookies.Values['auth_session'] ~= '') then
+			MODULE.Account.Status = asValid
+			return true
+		else
+			MODULE.Account.Status = asInvalid
+			return false
+		end
+	else
+		MODULE.Account.Status = asUnknown
+		return false
+	end
+end
+
 -- Get links and names from the manga list of the current website.
 function _M.GetNameAndLink()
 	local v, x = nil
@@ -48,14 +70,20 @@ function _M.GetInfo()
 	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('seriesStatus', json), 'COMING_SOON|HIATUS|MASS_RELEASED|ONGOING', 'CANCELLED|COMPLETED|DROPPED')
 	MANGAINFO.Summary   = x.XPathString('postContent', json)
 
-	if HTTP.GET(MODULE.RootURL .. '/api/chapters?postId=' .. x.XPathString('id', json) .. '&skip=0&take=9999&order=asc') then
-		x = CreateTXQuery(HTTP.Document)
-		for v in x.XPath('json(*).post.chapters()').Get() do
-			title = v.GetProperty('title').ToString()
-			title = title ~= 'null' and title ~= '' and string.format(' - %s', title) or ''
+	if not HTTP.GET(MODULE.RootURL .. '/api/chapters?postId=' .. x.XPathString('id', json) .. '&skip=0&take=9999&order=asc') then return net_problem end
 
+	for v in CreateTXQuery(HTTP.Document).XPath('json(*).post.chapters()').Get() do
+		title = v.GetProperty('title').ToString()
+		title = title ~= 'null' and title ~= '-' and title ~= '' and string.format(' - %s', title) or ''
+
+		if MODULE.GetOption('showpaidchapters') then
 			MANGAINFO.ChapterLinks.Add('series/' .. x.XPathString('slug', json) .. '/' .. v.GetProperty('slug').ToString())
 			MANGAINFO.ChapterNames.Add('Chapter ' .. v.GetProperty('number').ToString() .. title)
+		else
+			if v.GetProperty('isLocked').ToString() ~= 'true' then
+				MANGAINFO.ChapterLinks.Add('series/' .. x.XPathString('slug', json) .. '/' .. v.GetProperty('slug').ToString())
+				MANGAINFO.ChapterNames.Add('Chapter ' .. v.GetProperty('number').ToString() .. title)
+			end
 		end
 	end
 
