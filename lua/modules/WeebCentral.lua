55e4a1e2881316b49f5d94d9916a3a337a0ac79c
Slasar41 <agungtrianto41@outlook.com>
2025-01-09 00:24:18 +0700

    WeebCentral: Refactor code

--- lua/modules/WeebCentral.lua
+++ lua/modules/WeebCentral.lua
@@ -30,7 +30,7 @@ DirectoryPageLimit = 32
 function GetDirectoryPageNumber()
 	local s, x = nil
 	local u = MODULE.RootURL .. DirectoryPagination .. 0
-	PAGENUMBER = 8300
+	PAGENUMBER = 8500
 
 	if not HTTP.GET(u .. PAGENUMBER) then return net_problem end
 
@@ -65,7 +65,7 @@ end
 
 -- Get info and chapter list for the current manga.
 function GetInfo()
-	local s, v, x = nil
+	local v, x = nil
 	local u = MaybeFillHost(MODULE.RootURL, URL)
 
 	if not HTTP.GET(u) then return net_problem end
@@ -75,21 +75,15 @@ function GetInfo()
 	MANGAINFO.CoverLink = x.XPathString('//meta[@property="og:image"]/@content')
 	MANGAINFO.Authors   = x.XPathStringAll('//li[contains(., "Author")]/span/a')
 	MANGAINFO.Genres    = x.XPathStringAll('//li[contains(., "Tags")]/span/a')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//li[contains(., "Status")]/a'))
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//li[contains(., "Status")]/a'), 'Hiatus|Ongoing', 'Canceled|Complete')
 	MANGAINFO.Summary   = x.XPathString('//li[contains(., "Description")]/p')
 
-	s = x.XPathString('//button[@hx-target="#chapter-list"]/@hx-get')
-	if s == '' then
-		for v in x.XPath('//div[@id="chapter-list"]/a[not(@aria-label)]').Get() do
-			MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
-			MANGAINFO.ChapterNames.Add(x.XPathString('span[2]/span[1]', v))
-		end
-	elseif HTTP.GET(s) then
-		x = CreateTXQuery(HTTP.Document)
-		for v in x.XPath('//a[not(@aria-label)]').Get() do
-			MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
-			MANGAINFO.ChapterNames.Add(x.XPathString('span[2]/span[1]', v))
-		end
+	u = MANGAINFO.URL:gsub('(.*/series/.*)/.*', '%1') .. '/full-chapter-list'
+	if not HTTP.GET(u) then return net_problem end
+	x = CreateTXQuery(HTTP.Document)
+	for v in x.XPath('//a[not(@aria-label)]').Get() do
+		MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
+		MANGAINFO.ChapterNames.Add(x.XPathString('span[2]/span[1]', v))
 	end
 	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
@@ -98,19 +92,11 @@ end
 
 -- Get the page count for the current chapter.
 function GetPageNumber()
-	local data_cdn, data_length, x = nil
-	local u = MaybeFillHost(MODULE.RootURL, URL)
+	local u = MaybeFillHost(MODULE.RootURL, URL) .. '/images?reading_style=long_strip'
 
 	if not HTTP.GET(u) then return net_problem end
 
-	x = CreateTXQuery(HTTP.Document)
-	data_cdn = x.XPathString('//link[@rel="preload"]/@href')
-	data_length = tonumber(x.XPathString('(//button[@class="w-full btn"])[last()]'))
-	if data_cdn and data_length then
-		for i = 1, data_length do
-			TASK.PageLinks.Add(data_cdn:gsub("%-001%.", string.format("-%03d.", i)))
-		end
-	end
+	CreateTXQuery(HTTP.Document).XPathStringAll('//img/@src', TASK.PageLinks)
 
 	return no_error
 end
