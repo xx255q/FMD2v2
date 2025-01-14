d9ea7a9f4a9be54c8e0d87ecb63a78fc65db1427
Slasar41 <agungtrianto41@outlook.com>
2025-01-14 23:30:54 +0700

    HentaiLib: Added website support (#1951)

---
+++ lua/modules/HentaiLib.lua
@@ -0,0 +1,73 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                    = '17da8b4f514d418097801bfe9c88feab'
+	m.Name                  = 'HentaiLib'
+	m.RootURL               = 'https://hentailib.me'
+	m.Category              = 'H-Sites'
+	m.OnGetNameAndLink      = 'GetNameAndLink'
+	m.OnGetInfo             = 'GetInfo'
+	m.OnGetPageNumber       = 'GetPageNumber'
+	m.SortedList            = true
+
+	local fmd = require 'fmd.env'
+	local slang = fmd.SelectedLanguage
+	local lang = {
+		['en'] = {
+			['auth'] = 'Authorization:',
+			['showscangroup'] = 'Show scanlation group',
+			['isvr'] = 'Image server:',
+			['svr'] = 'Main\nSecondary\nCompress'
+		},
+		['ru_RU'] = {
+			['auth'] = 'Авторизация:',
+			['showscangroup'] = 'Показать группу сканлейт',
+			['isvr'] = 'Сервер изображений:',
+			['svr'] = 'Первый\nВторой\nСжатия'
+		},
+		get =
+			function(self, key)
+				local sel = self[slang]
+				if sel == nil then sel = self['en'] end
+				return sel[key]
+			end
+	}
+	m.AddOptionComboBox('svr', lang:get('isvr'), lang:get('svr'), 0)
+	m.AddOptionCheckBox('showscangroup', lang:get('showscangroup'), false)
+	m.AddOptionEdit('auth', lang:get('auth'))
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.LibGroup'
+SITE_ID = '4'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for the current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return no_error
+end
