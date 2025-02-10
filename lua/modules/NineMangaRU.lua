71c68f0c26c85014761d3cd827f2981571ff8140
Slasar41 <agungtrianto41@outlook.com>
2025-02-10 14:16:14 +0700

    NineMangaRU: Fixed chapter download (#2046)

--- lua/modules/NineMangaRU.lua
+++ lua/modules/NineMangaRU.lua
@@ -11,7 +11,6 @@ function Init()
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
 	m.OnGetPageNumber          = 'GetPageNumber'
-	m.OnGetImageURL            = 'GetImageURL'
 end
 
 ----------------------------------------------------------------------------------------------------
@@ -44,14 +43,28 @@ end
 
 -- Get the page count for the current chapter.
 function GetPageNumber()
-	Template.GetPageNumber()
+	local cookie_name, last_location, x = nil
+	local cid = URL:match('/(%d+).html$')
+	local u = 'https://www.mastertheenglish.com'
+	local p = '/go/jump/?type=runinemanga&cid=' .. cid
+	local w = 'https://www.financewealthloans.com/go/runm/' .. cid
+
+	HTTP.Headers.Values['Referer'] = w
+
+	if not HTTP.GET(u .. p) then return net_problem end
+
+	last_location = HTTP.Document.ToString():match('window%.location%.href="(.*)";')
+	cookie_name = 'lrgarden_visit_check_' .. last_location:match('(%d+)')
+
+	HTTP.Reset()
+	HTTP.Headers.Values['Referer'] = w
+	HTTP.Cookies.Values[cookie_name] = cid
+
+	if not HTTP.GET(u .. last_location) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	x.ParseHTML('[' .. GetBetween('all_imgs_url: [', '],', x.XPathString('//script[contains(., "all_imgs_url")]')) .. ']')
+	x.XPathStringAll('json(*)()', TASK.PageLinks)
 
 	return no_error
-end
-
--- Extract/Build/Repair image urls before downloading them.
-function GetImageURL()
-	Template.GetImageURL()
-
-	return true
 end
