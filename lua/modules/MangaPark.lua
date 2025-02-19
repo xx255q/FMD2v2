62ce59020452aaee92102f33e9d8b7463837760e
Slasar41 <agungtrianto41@outlook.com>
2025-02-19 22:12:46 +0700

    MangaPark: Added more mirrors (#2070)

--- lua/modules/MangaPark.lua
+++ lua/modules/MangaPark.lua
@@ -3,24 +3,32 @@
 ----------------------------------------------------------------------------------------------------
 
 function Init()
-	function AddWebsiteModule(id, name, url)
+	function AddWebsiteModule(id, url, cat)
 		local m = NewWebsiteModule()
 		m.ID                       = id
-		m.Name                     = name
+		m.Name                     = 'MangaPark'
 		m.RootURL                  = url
-		m.Category                 = 'English'
+		m.Category                 = cat
 		m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
 		m.OnGetNameAndLink         = 'GetNameAndLink'
 		m.OnGetInfo                = 'GetInfo'
 		m.OnGetPageNumber          = 'GetPageNumber'
 		m.SortedList               = true
 	end
-	AddWebsiteModule('32b25f04931b414a803286a6f8cabd8d', 'MangaParkNet', 'https://mangapark.net')
-	AddWebsiteModule('09d49c7760904bc2817dbc80757de44a', 'MangaParkMe', 'https://mangapark.me')
-	AddWebsiteModule('273268fd7b1a42b38e50c010a630bfad', 'MangaParkCom', 'https://mangapark.com')
-	AddWebsiteModule('2784b193e1794dcdac1f68eb59e9ad0c', 'MangaParkOrg', 'https://mangapark.org')
-	AddWebsiteModule('885424a90b764a0894dad0d0a1c4e545', 'MangaParkIo', 'https://mangapark.io')
-	AddWebsiteModule('b14a022f3c4e4a72b47cb89345adf9c4', 'MangaParkTo', 'https://mangapark.to')
+	AddWebsiteModule('32b25f04931b414a803286a6f8cabd8d', 'https://mangapark.net', 'English')
+	AddWebsiteModule('09d49c7760904bc2817dbc80757de44a', 'https://mangapark.me')
+	AddWebsiteModule('273268fd7b1a42b38e50c010a630bfad', 'https://mangapark.com')
+	AddWebsiteModule('2784b193e1794dcdac1f68eb59e9ad0c', 'https://mangapark.org')
+	AddWebsiteModule('885424a90b764a0894dad0d0a1c4e545', 'https://mangapark.io')
+	AddWebsiteModule('b14a022f3c4e4a72b47cb89345adf9c4', 'https://mangapark.to')
+	AddWebsiteModule('44f8204959644aeb98d14f66f8f7d817', 'https://comicpark.org')
+	AddWebsiteModule('d197cecff2f443debdc925d567c345e5', 'https://comicpark.to')
+	AddWebsiteModule('4dd7f91c86be48b38284e4c47e0f3c7a', 'https://readpark.org')
+	AddWebsiteModule('1ab8d1a539e445bc8e5c0f533875ee9f', 'https://readpark.net')
+	AddWebsiteModule('15994152f69b4b8188cc93e8b13d4f34', 'https://parkmanga.com')
+	AddWebsiteModule('600f0a75aada4b68b11efd98cc2f9c4a', 'https://parkmanga.net')
+	AddWebsiteModule('cbf375bed7fa423ead7cc2764b2ff514', 'https://parkmanga.org')
+	AddWebsiteModule('15b0c0c7dbe84382928e1ea2ccae076b', 'https://mpark.to')
 end
 
 ----------------------------------------------------------------------------------------------------
@@ -66,34 +74,36 @@ function GetInfo()
 	local v, x = nil
 	local crypto = require 'fmd.crypto'
 	local u = MODULE.RootURL .. API_URL
-	local s = '{"query":"{ get_comicNode( id: ' .. URL:match('(%d+)') .. ' ) { data { id name artists authors genres originalStatus uploadStatus summary urlCoverOri } } }"}' 
+	local s = '{"query":"{ get_comicNode( id: ' .. URL:match('(%d+)') .. ' ) { data { name altNames urlCoverOri authors artists genres uploadStatus summary } } }"}' 
 	HTTP.MimeType = 'application/json'
 
 	if not HTTP.POST(u, s) then return net_problem end
 
 	x = CreateTXQuery(crypto.HTMLEncode(HTTP.Document.ToString()))
-	MANGAINFO.Title     = x.XPathString('json(*).data.get_comicNode.data.name')
-	MANGAINFO.CoverLink = x.XPathString('json(*).data.get_comicNode.data.urlCoverOri')
-	MANGAINFO.Authors   = x.XPathStringAll('json(*).data.get_comicNode.data.authors()')
-	MANGAINFO.Artists   = x.XPathStringAll('json(*).data.get_comicNode.data.artists()')
-	MANGAINFO.Genres    = x.XPathStringAll('json(*).data.get_comicNode.data.genres()'):gsub("_", " ")
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('json(*).data.get_comicNode.data.originalStatus'), 'ongoing|hiatus|pending', 'completed|cancelled')
-	MANGAINFO.Summary   = x.XPathString('json(*).data.get_comicNode.data.summary')
+	json = x.XPath('json(*).data.get_comicNode.data')
+	MANGAINFO.Title     = x.XPathString('name', json)
+	MANGAINFO.AltTitles = x.XPathString('string-join(altNames?*, ", ")', json)
+	MANGAINFO.CoverLink = x.XPathString('urlCoverOri', json)
+	MANGAINFO.Authors   = x.XPathString('string-join(authors?*, ", ")', json)
+	MANGAINFO.Artists   = x.XPathString('string-join(artists?*, ", ")', json)
+	MANGAINFO.Genres    = x.XPathString('string-join(genres?*, ", ")', json):gsub("_", " ")
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('uploadStatus', json))
+	MANGAINFO.Summary   = x.XPathString('summary', json)
 
 	HTTP.Reset()
 	s = '{"query":"{ get_comicChapterList( comicId: ' .. URL:match('(%d+)') .. ' ) { data { id dname title } } }"}'
 	HTTP.MimeType = 'application/json'
-	if HTTP.POST(u, s) then
-		x = CreateTXQuery(HTTP.Document)
-		for v in x.XPath('json(*).data.get_comicChapterList()').Get() do
-			chapter = x.XPathString('data/dname', v)
-			title = x.XPathString('data/title', v)
 
-			title = title ~= 'null' and title ~= '' and string.format(' - %s', title) or ''
+	if not HTTP.POST(u, s) then return net_problem end
 
-			MANGAINFO.ChapterLinks.Add(x.XPathString('data/id', v))
-			MANGAINFO.ChapterNames.Add(chapter .. title)
-		end
+	x = CreateTXQuery(HTTP.Document)
+	for v in x.XPath('json(*).data.get_comicChapterList()').Get() do
+		chapter = x.XPathString('data/dname', v)
+		title = x.XPathString('data/title', v)
+		title = title ~= 'null' and title ~= '' and string.format(' - %s', title) or ''
+
+		MANGAINFO.ChapterLinks.Add(x.XPathString('data/id', v))
+		MANGAINFO.ChapterNames.Add(chapter .. title)
 	end
 
 	return no_error
