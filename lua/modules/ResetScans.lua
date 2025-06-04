a4faa9ea0bbad0b54eb6a1440f4a2368518a8c45
Slasar41 <agungtrianto41@outlook.com>
2025-06-04 23:50:48 +0700

    Reset Scans: Changed domain and fixed chapter list (#2297)

--- lua/modules/ResetScans.lua
+++ lua/modules/ResetScans.lua
@@ -6,7 +6,7 @@ function Init()
 	local m = NewWebsiteModule()
 	m.ID                       = '287f665620664e468d4e05f5d76f5a43'
 	m.Name                     = 'Reset Scans'
-	m.RootURL                  = 'https://reset-scans.co'
+	m.RootURL                  = 'https://reset-scans.org'
 	m.Category                 = 'English-Scanlation'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
@@ -32,7 +32,23 @@ end
 
 -- Get info and chapter list for the current manga.
 function GetInfo()
-	Template.GetInfo()
+	local x = nil
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	MANGAINFO.Title     = x.XPathString('//div[@class="post-title"]/h1')
+	MANGAINFO.AltTitles = x.XPathString('//div[@class="summary-heading" and ./h5="Alternative"]/following-sibling::div')
+	MANGAINFO.CoverLink = x.XPathString('//div[@class="summary_image"]//img/@src')
+	MANGAINFO.Authors   = x.XPathStringAll('//div[@class="author-content"]/a')
+	MANGAINFO.Artists   = x.XPathStringAll('//div[@class="artist-content"]/a')
+	MANGAINFO.Genres    = x.XPathStringAll('//div[@class="genres-content"]/a')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//div[@class="summary-heading" and ./h5="Status"]/following-sibling::div'))
+	MANGAINFO.Summary   = x.XPathString('//div[@class="summary__content"]')
+
+	x.XPathHREFAll('//div[@class="li__text"]/a[not(@href="#")]', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
+	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
 	return no_error
 end
@@ -41,5 +57,5 @@ end
 function GetPageNumber()
 	Template.GetPageNumber()
 
-	return no_error
+	return true
 end
