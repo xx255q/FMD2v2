fbcc1e221ae480ca02c4ea07cb0da42947fce5b6
Slasar41 <agungtrianto41@outlook.com>
2025-06-21 23:00:21 +0700

    WebToons: Fixed manga info and refactor code (#2319)

--- lua/modules/WebToons.lua
+++ lua/modules/WebToons.lua
@@ -1,3 +1,49 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = '18f636ec7fdf47fabe95d940ad0b548f'
+	m.Category                 = 'English'
+	m.Name                     = 'WebToons'
+	m.RootURL                  = 'https://www.webtoons.com/'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+	m.OnBeforeDownloadImage    = 'BeforeDownloadImage'
+	m.AddServerCookies('webtoons.com', 'ageGatePass=True; max-age=31556952')
+
+	local fmd = require 'fmd.env'
+	local slang = fmd.SelectedLanguage
+	local lang = {
+		['en'] = {
+			['includechallengetitles'] = 'Include manga titles from WebToons Challenge (takes very very long to create manga list!)',
+			['lang'] = 'Language:'
+		},
+		['id_ID'] = {
+			['includechallengetitles'] = 'Sertakan judul komik dari WebToons Challenge (perlu waktu yang sangat lama untuk membuat daftar komik!)',
+			['lang'] = 'Bahasa:'
+		},
+		get =
+			function(self, key)
+				local sel = self[slang]
+				if sel == nil then sel = self['en'] end
+				return sel[key]
+			end
+	}
+	m.AddOptionCheckBox('luaincludechallengetitles', lang:get('includechallengetitles'), false)
+
+	local items = 'All'
+	local t = GetLangList()
+	for k, v in ipairs(t) do items = items .. '\r\n' .. v; end
+	m.AddOptionComboBox('lualang', lang:get('lang'), items, 2)
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
 local langs = {
 	["en"] = "English",
 	["id"] = "Indonesian",
@@ -5,51 +51,49 @@ local langs = {
 	["th"] = "Thai"
 }
 
-function getinfo()
-	MANGAINFO.URL = MANGAINFO.URL:gsub('(.*)&page=.*', '%1')
-	HTTP.Headers.Values['sec-ch-ua-platform'] = '"Android"'
-	HTTP.Headers.Values['sec-ch-ua-platform-version'] = '"6.0"'
-	if HTTP.GET(MANGAINFO.URL) then
-		x=CreateTXQuery(HTTP.Document)
-		MANGAINFO.Title = x.XPathString('//meta[@property="og:title"]/@content')
-		MANGAINFO.CoverLink=x.XPathString('//meta[@name="twitter:image"]/@content')
-		MANGAINFO.Authors=x.XPathString('//div[contains(@class, "detail_info")]/p[@class="author"]/text()'):gsub("[^%a%d ,]", '')
-		MANGAINFO.Genres=x.XPathString('//div[@class="info"]/h2')
-		MANGAINFO.Status = MangaInfoStatusIfPos(x.XPathString('//div[@class="info_update"]/text()[1]'), 'every')
-		if MANGAINFO.Genres == '' then
-			MANGAINFO.Genres=x.XPathString('//div[contains(@class, "detail_info")]/p[@class="genre"]')
-		end
-		MANGAINFO.Summary=x.XPathString('//p[@class="summary"]')
-		local v
-		for v in x.XPath('//ul[@id="_episodeList"]/li[@class="_episodeItem"]/a').Get() do
-			MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
-			MANGAINFO.ChapterNames.Add(x.XPathString('.//p/span[@class="ellipsis"]', v))
-		end
-		MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
-		return no_error
+----------------------------------------------------------------------------------------------------
+-- Auxiliary Functions
+----------------------------------------------------------------------------------------------------
+
+function GetLang(lang)
+	if langs[lang] ~= nil then
+		return langs[lang]
 	else
-		return net_problem
+		return 'Unknown'
 	end
 end
 
-function getpagenumber()
-	TASK.PageNumber=0
-	TASK.PageLinks.Clear()
-	URL = URL:gsub('(.*)&page=.*', '%1')
-	HTTP.Cookies.Values['ageGatePass'] = 'True'
-	if HTTP.GET(MaybeFillHost(MODULE.RootURL,URL)) then
-		CreateTXQuery(HTTP.Document).XPathStringAll('//div[@id="_imageList"]/img[@class="_images"]/@data-URL', TASK.PageLinks)
-		return true
-	else
-		return false
-	end
+function GetLangList()
+	local t = {}
+	for k, v in pairs(langs) do table.insert(t, v); end
+	table.sort(t)
+	return t
 end
 
-function getnameandlink()
+function FindLang(lang)
+	local t = GetLangList()
+	for i, v in ipairs(t) do
+		if i == lang then
+			lang = v
+			break
+		end
+	end
+	for k, v in pairs(langs) do
+		if v == lang then return k; end
+	end
+	return nil
+end
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
 	local selectedLang = MODULE.GetOption('lualang')
 	local l = langs
 	if selectedLang > 0 then
-		l = {[findlang(selectedLang)] = ""}
+		l = {[FindLang(selectedLang)] = ""}
 	end
 	local key, dirurl, v
 	local x = CreateTXQuery()
@@ -93,74 +137,49 @@ function getnameandlink()
 	return no_error
 end
 
-function BeforeDownloadImage()
-	HTTP.Headers.Values['Referer'] = URL
+-- Get info and chapter list for the current manga.
+function GetInfo()
+	local v, x = nil
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	MANGAINFO.Title     = x.XPathString('//meta[@property="og:title"]/@content')
+	MANGAINFO.CoverLink = x.XPathString('//meta[@property="og:image"]/@content')
+	MANGAINFO.Authors   = x.XPathString('//div[@class="author_area"]/text()'):gsub('[^%a%d ,]', '')
+	MANGAINFO.Genres    = x.XPathString('//h2[contains(@class, "genre")]')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//p[@class="day_info"]'), 'UP')
+	MANGAINFO.Summary   = x.XPathString('//p[@class="summary"]')
+
+	u = 'https://m.webtoons.com/api/v1/webtoon/' .. URL:match('title_no=(%d+)') .. '/episodes?pageSize=99999'
+
+	if not HTTP.GET(u) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	for v in x.XPath('json(*).result.episodeList()').Get() do
+		MANGAINFO.ChapterLinks.Add(v.GetProperty('viewerLink').ToString())
+		MANGAINFO.ChapterNames.Add(v.GetProperty('episodeTitle').ToString())
+	end
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+	HTTP.Cookies.Values['ageGatePass'] = 'True'
+
+	if not HTTP.GET(u) then return false end
+
+	CreateTXQuery(HTTP.Document).XPathStringAll('//div[@id="_imageList"]/img[@class="_images"]/@data-URL', TASK.PageLinks)
+
 	return true
 end
 
-function getlang(lang)
-	if langs[lang] ~= nil then
-		return langs[lang]
-	else
-		return 'Unknown'
-	end
-end
+-- Prepare the URL, http header and/or http cookies before downloading an image.
+function BeforeDownloadImage()
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL
 
-function getlanglist()
-	local t = {}
-	for k, v in pairs(langs) do table.insert(t, v); end
-	table.sort(t)
-	return t
-end
-
-function findlang(lang)
-	local t = getlanglist()
-	for i, v in ipairs(t) do
-		if i == lang then
-			lang = v
-			break
-		end
-	end
-	for k, v in pairs(langs) do
-		if v == lang then return k; end
-	end
-	return nil
-end
-
-function Init()
-	local m = NewWebsiteModule()
-	m.ID                    = '18f636ec7fdf47fabe95d940ad0b548f'
-	m.Category              ='English'
-	m.Name                  ='WebToons'
-	m.RootURL               ='https://www.webtoons.com/'
-	m.OnGetInfo             ='getinfo'
-	m.OnGetPageNumber       ='getpagenumber'
-	m.OnGetNameAndLink      ='getnameandlink'
-	m.OnBeforeDownloadImage = 'BeforeDownloadImage'
-	m.AddServerCookies('webtoons.com', 'ageGatePass=True; max-age=31556952')
-
-	local fmd = require 'fmd.env'
-	local slang = fmd.SelectedLanguage
-	local lang = {
-		['en'] = {
-			['includechallengetitles'] = 'Include manga titles from WebToons Challenge (takes very very long to create manga list!)',
-			['lang'] = 'Language:'
-		},
-		['id_ID'] = {
-			['includechallengetitles'] = 'Sertakan judul komik dari WebToons Challenge (perlu waktu yang sangat lama untuk membuat daftar komik!)',
-			['lang'] = 'Bahasa:'
-		},
-		get =
-			function(self, key)
-				local sel = self[slang]
-				if sel == nil then sel = self['en'] end
-				return sel[key]
-			end
-	}
-	m.AddOptionCheckBox('luaincludechallengetitles', lang:get('includechallengetitles'), false)
-
-	local items = 'All'
-	local t = getlanglist()
-	for k, v in ipairs(t) do items = items .. '\r\n' .. v; end
-	m.AddOptionComboBox('lualang', lang:get('lang'), items, 2)
+	return true
 end
