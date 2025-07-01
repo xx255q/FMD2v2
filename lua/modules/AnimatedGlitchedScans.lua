bd484f9604d207392244c21f98a3b724040cc017
Slasar41 <agungtrianto41@outlook.com>
2025-07-02 00:49:12 +0700

    AGR Comics: Changed domain and template (#2336)

--- lua/modules/AnimatedGlitchedScans.lua
+++ lua/modules/AnimatedGlitchedScans.lua
@@ -6,11 +6,13 @@ function Init()
 	local m = NewWebsiteModule()
 	m.ID                       = '50868d7d8eaf4b8d948efa7333ed2989'
 	m.Name                     = 'AGR Comics'
-	m.RootURL                  = 'https://agrcomics.com'
+	m.RootURL                  = 'https://agrcomics.org'
 	m.Category                 = 'English-Scanlation'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
 	m.OnGetPageNumber          = 'GetPageNumber'
+	m.OnLogin                  = 'Login'
+	m.AccountSupport           = true
 
 	local fmd = require 'fmd.env'
 	local slang = fmd.SelectedLanguage
@@ -35,11 +37,19 @@ end
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-local Template = require 'templates.KeyoApp'
+local Template = require 'templates.Iken'
+API_URL = 'https://api.agrcomics.org'
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
 ----------------------------------------------------------------------------------------------------
+
+-- Sign in to the current website.
+function Login()
+	Template.Login()
+
+	return no_error
+end
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
@@ -48,7 +58,7 @@ function GetNameAndLink()
 	return no_error
 end
 
--- Get info and chapter list for current manga.
+-- Get info and chapter list for the current manga.
 function GetInfo()
 	Template.GetInfo()
 
@@ -59,5 +69,5 @@ end
 function GetPageNumber()
 	Template.GetPageNumber()
 
-	return no_error
+	return true
 end
