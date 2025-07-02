0ab73cca3e1a0daa181339b73ee747f5b01006ed
Slasar41 <agungtrianto41@outlook.com>
2025-07-02 22:59:20 +0700

    Kira Scans: Added website support (#2338)

---
+++ lua/modules/KiraScans.lua
@@ -0,0 +1,56 @@
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
+
+function Init()
+	local m = NewWebsiteModule()
+	m.ID                       = 'ebdcef553a0f4fb88e16480ff875304c'
+	m.Name                     = 'Kira Scans'
+	m.RootURL                  = 'https://kirascans.com'
+	m.Category                 = 'English-Scanlation'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.MangaThemesia'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for the current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	local v, x = nil
+
+	x = CreateTXQuery(HTTP.Document)
+	MANGAINFO.AltTitles = x.XPathString('//div[@class="desktop-titles"]')
+
+	for v in x.XPath('//div[@id="chapterlist"]//li/a[not(@data-bs-target="#lockedChapterModal")]').Get() do
+		MANGAINFO.ChapterLinks.Add(v.GetAttribute('href'))
+		MANGAINFO.ChapterNames.Add(x.XPathString('.//span[@class="chapternum"]/normalize-space(.)', v))
+	end
+	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return true
+end
