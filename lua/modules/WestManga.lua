89ec69b7351291a70ea6f77f8a1bfd5870dc5db6
Slasar41 <agungtrianto41@outlook.com>
2025-07-03 09:21:44 +0700

    WestManga: Changed template (#2340)

--- lua/modules/WestManga.lua
+++ lua/modules/WestManga.lua
@@ -8,6 +8,7 @@ function Init()
 	m.Name                     = 'WestManga'
 	m.RootURL                  = 'https://westmanga.me'
 	m.Category                 = 'Indonesian'
+	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
 	m.OnGetPageNumber          = 'GetPageNumber'
@@ -17,29 +18,93 @@ end
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-local Template = require 'templates.MangaThemesia'
+local API_URL = 'https://westmanga.me/api'
+local DirectoryPagination = '/contents?page=%s&per_page=40&orderBy=Added&type=Comic'
+local AccessKey = 'WM_WEB_FRONT_END'
+local SecretKey = 'xxxoidj'
+local sha256 = require('utils.sha256')
+
+----------------------------------------------------------------------------------------------------
+-- Auxiliary Functions
+----------------------------------------------------------------------------------------------------
+
+-- Sets the required HTTP headers for making an API request.
+function SetApiHeaders(RequestPath)
+	local timestamp = os.time()
+	local key = timestamp .. 'GET' .. RequestPath .. AccessKey .. SecretKey
+	HTTP.Headers.Values['x-wm-accses-key'] = AccessKey
+	HTTP.Headers.Values['x-wm-request-time'] = timestamp
+	HTTP.Headers.Values['x-wm-request-signature'] = sha256.hmac_sha256(key, 'wm-api-request')
+end
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
 ----------------------------------------------------------------------------------------------------
 
+-- Get the page count of the manga list of the current website.
+function GetDirectoryPageNumber()
+	local u = API_URL .. DirectoryPagination:format(1)
+	SetApiHeaders('/api' .. DirectoryPagination:format(1))
+
+	if not HTTP.GET(u) then return net_problem end
+
+	PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('json(*).paginator.last_page')) or 1
+
+	return no_error
+end
+
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
-	Template.GetNameAndLink()
+	local v, x = nil
+	local u = API_URL .. DirectoryPagination:format(URL + 1)
+	SetApiHeaders('/api' .. DirectoryPagination:format(URL + 1))
+
+	if not HTTP.GET(u) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	for v in x.XPath('json(*).data()').Get() do
+		LINKS.Add('comic/' .. v.GetProperty('slug').ToString())
+		NAMES.Add(v.GetProperty('title').ToString())
+	end
 
 	return no_error
 end
 
 -- Get info and chapter list for the current manga.
 function GetInfo()
-	Template.GetInfo()
+	local json, v, x = nil
+	local u = API_URL .. URL
+	SetApiHeaders('/api' .. URL)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	x = CreateTXQuery(HTTP.Document)
+	json = x.XPath('json(*).data')
+	MANGAINFO.Title     = x.XPathString('title', json)
+	MANGAINFO.AltTitles = x.XPathString('alternative_name', json)
+	MANGAINFO.CoverLink = x.XPathString('cover', json)
+	MANGAINFO.Authors   = x.XPathString('author', json)
+	MANGAINFO.Genres    = x.XPathStringAll('json(*).data.genres().name')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('status', json))
+	MANGAINFO.Summary   = x.XPathString('sinopsis', json)
+
+	for v in x.XPath('json(*).data.chapters()').Get() do
+		MANGAINFO.ChapterLinks.Add(v.GetProperty('slug').ToString())
+		MANGAINFO.ChapterNames.Add('Chapter ' .. v.GetProperty('number').ToString())
+	end
+	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
 	return no_error
 end
 
 -- Get the page count for the current chapter.
 function GetPageNumber()
-	Template.GetPageNumber()
+	local u = API_URL .. '/v' .. URL
+	SetApiHeaders('/api/v' .. URL)
+
+	if not HTTP.GET(u) then return false end
+
+	CreateTXQuery(HTTP.Document).XPathStringAll('json(*).data.images()', TASK.PageLinks)
 
 	return true
 end
