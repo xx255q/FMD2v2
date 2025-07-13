a5fa26ac0f25a4824ff8699ddc2be3571e117847
Slasar41 <agungtrianto41@outlook.com>
2025-07-13 14:42:26 +0700

    GroupLe: Added account support (#2361)

--- lua/templates/GroupLe.lua
+++ lua/templates/GroupLe.lua
@@ -12,10 +12,40 @@ DirectoryPagination = '/list?sortType=DATE_CREATE'
 DirectoryParameters = '&offset='
 DirectoryOffset     = 50
 KeepParameters      = false
+LoginUrl            = 'https://3.grouple.co'
+SITE_ID             = ''
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
 ----------------------------------------------------------------------------------------------------
+
+-- Sign in to the current website.
+function _M.Login()
+	local s, x = nil
+	local crypto = require 'fmd.crypto'
+	local login_url = LoginUrl .. '/login/authenticate?ttt=' .. os.time() .. '&siteId=' .. SITE_ID
+
+	if MODULE.Account.Enabled == false then return false end
+
+	s = 'targetUri=%2Flogin%2FcontinueSso%3FsiteId%3D' .. SITE_ID .. '%26targetUri%3D%252F' ..
+	'&username='  .. crypto.EncodeURLElement(MODULE.Account.Username) ..
+	'&password=' .. crypto.EncodeURLElement(MODULE.Account.Password) ..
+	'&remember_me=true&_remember_me_yes=&remember_me_yes=on'
+	MODULE.Account.Status = asChecking
+
+	if HTTP.POST(login_url, s) then
+		if (HTTP.ResultCode == 200) and (HTTP.Cookies.Values['remember_me'] ~= '') then
+			MODULE.Account.Status = asValid
+			return true
+		else
+			MODULE.Account.Status = asInvalid
+			return false
+		end
+	else
+		MODULE.Account.Status = asUnknown
+		return false
+	end
+end
 
 -- Get the page count of the manga list of the current website.
 function _M.GetDirectoryPageNumber()
@@ -59,14 +89,24 @@ function _M.GetInfo()
 	x.XPathHREFAll('//table[@class="table table-hover"]//a', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
 	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
 
+	HTTP.Reset()
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL
+
 	return no_error
 end
 
 -- Get the page count for the current chapter.
 function _M.GetPageNumber()
-	local domain, json, path = nil
+	local domain, json, user_hash, path = nil
 	local u = MaybeFillHost(MODULE.RootURL, URL)
-	if not URL:find('mtr=1') then u = u .. '?mtr=1' end
+
+	if MODULE.Account.Status == 2 or HTTP.Cookies.Values['remember_me'] ~= '' then
+		if not HTTP.GET(u) then return false end
+		user_hash = HTTP.Document.ToString():match("window%.user_hash = '(.-)';")
+		u = u .. '?mtr=true&d=' .. user_hash
+	else
+		u = u .. '?mtr=true'
+	end
 
 	if not HTTP.GET(u) then return false end
 
