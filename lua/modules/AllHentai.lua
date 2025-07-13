faaa0e1e871c333e2bde0a56d17112f20ee7a9d1
Slasar41 <agungtrianto41@outlook.com>
2025-07-13 14:58:10 +0700

    AllHentai: Fixed all (#2362)

--- lua/modules/AllHentai.lua
+++ lua/modules/AllHentai.lua
@@ -1,92 +1,73 @@
-local dirurl = '/list?type=&sortType=DATE_CREATE'
-local perpage = 60
-
-function getinfo()
-	MANGAINFO.URL=MaybeFillHost(MODULE.RootURL, URL)
-	if HTTP.GET(MANGAINFO.URL) then
-		local x=CreateTXQuery(HTTP.Document)
-		local rname = x.XPathString('//meta[@itemprop="name"]/@content')
-		if MANGAINFO.Title == '' then
-			MANGAINFO.Title = x.XPathString('//meta[@itemprop="alternativeHeadline"]/@content')
-		end
-		if MANGAINFO.Title == '' then
-			MANGAINFO.Title = rname
-		end
-		MANGAINFO.CoverLink=MaybeFillHost(MODULE.RootURL, x.XPathString('//img[@itemprop="image"]/@src'))
-		MANGAINFO.Authors=x.XPathStringAll('//p[@class="elementList" and contains(b, "Автор")]/a')
-		MANGAINFO.Genres=x.XPathStringAll('//p[@class="elementList" and (contains(b, "Жанры") or contains(b, "Категории"))]/a')
-		MANGAINFO.Status = MangaInfoStatusIfPos(x.XPathString('//p[contains(b, "Перевод")]'), 'продолжается', 'завершен')
-		MANGAINFO.Summary=x.XPathString('//div[@class="mangaDescription"]/div[@itemprop="description"]')
-		-- TODO: remove manga name from chapter name
-		x.XPathHREFAll('//div[@class="expandable"]/table[@class="cTable"]/tbody/tr/td/a',MANGAINFO.ChapterLinks,MANGAINFO.ChapterNames)
-		MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
-		return no_error
-	else
-		return net_problem
-	end
-end
-
-function beforedownloadimage()
-	HTTP.Headers.Values['Referer'] = MODULE.RootURL
-	return true
-end
-
-function getpagenumber()
-	TASK.PageLinks.Clear()
-	if HTTP.GET(MaybeFillHost(MODULE.RootURL, URL)) then
-		local x=CreateTXQuery(HTTP.Document)
-		local s = x.XPathString('//script[contains(., "var pictures")]')
-		s = GetBetween('pictures =', ';', s)
-		x.ParseHTML(s)
-		x.XPathStringAll('json(*)().URL', TASK.PageLinks)
-	else
-		return false
-	end
-	return true
-end
-
-function getdirectorypagenumber()
-	if HTTP.GET(MODULE.RootURL .. dirurl) then
-		local x = CreateTXQuery(HTTP.Document)
-		local s = x.XPathString('//*[@class="pagination"]/a[@class="step"][last()]/@href')
-		PAGENUMBER = tonumber(s:match('offset=(%d+)')) or 1
-		if PAGENUMBER > 1 then
-		PAGENUMBER = math.ceil(PAGENUMBER / perpage) + 1
-	end
-		return no_error
-	else
-		return net_problem
-	end
-end
-
-function getnameandlink()
-	local s = MODULE.RootURL .. dirurl
-	if URL ~= '0' then s = s .. '&offset=' .. (tonumber(URL) * perpage) .. '&max=' .. perpage; end
-	if HTTP.GET(s) then
-		local x = CreateTXQuery(HTTP.Document)
-		local v = x.XPath('//table[@class="cTable"]//tr/td/a[not(@class)]')
-		for i = 1, v.Count do
-			local v1 = v.Get(i)
-			LINKS.Add(v1.GetAttribute('href'))
-			NAMES.Add(x.XPathString('./text()', v1))
-		end
-		return no_error
-	else
-		return net_problem
-	end
-end
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
 
 function Init()
 	local m = NewWebsiteModule()
-	m.ID = '29ef3c8373f54aa2b8167d2af7b0d241'
-	m.Name = 'AllHentai'
-	m.RootURL = 'http://allhentai.ru'
-	m.Category = 'Russian'
-	m.LastUpdated='April 26, 2018'
-	m.SortedList = true
-	m.OnGetInfo='getinfo'
-	m.OnGetPageNumber='getpagenumber'
-	m.OnGetNameAndLink='getnameandlink'
-	m.OnGetDirectoryPageNumber = 'getdirectorypagenumber'
-	m.OnBeforeDownloadImage = 'beforedownloadimage'
+	m.ID                       = '29ef3c8373f54aa2b8167d2af7b0d241'
+	m.Name                     = 'AllHentai'
+	m.RootURL                  = 'https://20.allhen.online'
+	m.Category                 = 'Russian'
+	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
+	m.OnBeforeDownloadImage    = 'BeforeDownloadImage'
+	m.OnLogin                  = 'Login'
+	m.AccountSupport           = true
+	m.SortedList               = true
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.GroupLe'
+LoginUrl = 'https://z.qawa.org'
+SITE_ID = '1'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Sign in to the current website.
+function Login()
+	Template.Login()
+
+	return no_error
+end
+
+-- Get the page count of the manga list of the current website.
+function GetDirectoryPageNumber()
+	Template.GetDirectoryPageNumber()
+
+	return no_error
+end
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	Template.GetNameAndLink()
+
+	return no_error
+end
+
+-- Get info and chapter list for the current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return true
+end
+
+-- Prepare the URL, http header and/or http cookies before downloading an image.
+function BeforeDownloadImage()
+	Template.BeforeDownloadImage()
+
+	return true
 end
