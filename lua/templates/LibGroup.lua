a57273e68083b18883670f5e6c4c631c59fbd2da
Slasar41 <agungtrianto41@outlook.com>
2025-08-01 02:23:11 +0700

    LibGroup: Added a referer to chapter download (#2410)

--- lua/templates/LibGroup.lua
+++ lua/templates/LibGroup.lua
@@ -18,15 +18,14 @@ SITE_ID = ''
 
 -- Get links and names from the manga list of the current website.
 function _M.GetNameAndLink()
-	local name, v, x = nil
 	local u = API_URL .. DirectoryPagination:format(SITE_ID) .. (URL + 1)
 
 	if not HTTP.GET(u) then return net_problem end
 
-	x = CreateTXQuery(HTTP.Document)
+	local x = CreateTXQuery(HTTP.Document)
 	if x.XPathString('json(*).meta.has_next_page') == 'false' then return no_error end
 	for v in x.XPath('json(*).data()').Get() do
-		name = v.GetProperty('rus_name').ToString()
+		local name = v.GetProperty('rus_name').ToString()
 		if name == '' then name = v.GetProperty('name').ToString() end
 
 		LINKS.Add('ru/manga/' .. v.GetProperty('slug_url').ToString())
@@ -39,7 +38,6 @@ end
 
 -- Get info and chapter list for the current manga.
 function _M.GetInfo()
-	local bid, branches, bteam, chapter, chapters, j, name, optgroup, scanlators, tags, team, teams, tename, tname, trname, v, volume, w, x = nil
 	local s = '?fields[]=tags&fields[]=authors&fields[]=artists&fields[]=genres&fields[]=status_id&fields[]=summary'
 	local slug = '/' .. URL:match('/manga/(.-)$'):gsub('(.*)?.*', '%1')
 	local u = API_URL .. '/manga' .. slug
@@ -47,7 +45,7 @@ function _M.GetInfo()
 
 	if not HTTP.GET(u .. s) then return net_problem end
 
-	x = CreateTXQuery(require 'fmd.crypto'.HTMLEncode(HTTP.Document.ToString()))
+	local x = CreateTXQuery(require 'fmd.crypto'.HTMLEncode(HTTP.Document.ToString()))
 	MANGAINFO.CoverLink = x.XPathString('json(*).data.cover.thumbnail')
 	MANGAINFO.Authors   = x.XPathStringAll('json(*).data.authors().name')
 	MANGAINFO.Artists   = x.XPathStringAll('json(*).data.artists().name')
@@ -55,9 +53,9 @@ function _M.GetInfo()
 	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('json(*).data.scanlateStatus.label'), 'Продолжается', 'Завершён', 'Заморожен', 'Заброшен')
 	MANGAINFO.Summary   = x.XPathString('json(*).data.summary')
 
-	tname = x.XPathString('json(*).data.name')
-	trname = x.XPathString('json(*).data.rus_name')
-	tename = x.XPathString('json(*).data.eng_name')
+	local tname = x.XPathString('json(*).data.name')
+	local trname = x.XPathString('json(*).data.rus_name')
+	local tename = x.XPathString('json(*).data.eng_name')
 	MANGAINFO.Title = trname
 	if trname == '' then MANGAINFO.Title = tname end
 	MANGAINFO.AltTitles = ''
@@ -68,33 +66,33 @@ function _M.GetInfo()
 		MANGAINFO.AltTitles = tename
 	end
 
-	tags = x.XPathStringAll('json(*).data.tags().name')
+	local tags = x.XPathStringAll('json(*).data.tags().name')
 	if tags ~= '' then MANGAINFO.Genres = MANGAINFO.Genres .. ', ' .. tags end
 
-	optgroup = MODULE.GetOption('showscangroup')
+	local optgroup = MODULE.GetOption('showscangroup')
 	HTTP.Reset()
 	HTTP.Headers.Values['Authorization'] = MODULE.GetOption('auth')
 	if HTTP.GET(u .. '/chapters') then
-		x = CreateTXQuery(HTTP.Document)
-		chapters = x.XPath('json(*).data()')
+		local x = CreateTXQuery(HTTP.Document)
+		local chapters = x.XPath('json(*).data()')
 		for i = 1, chapters.Count do
-			v = chapters.Get(i)
+			local v = chapters.Get(i)
 
-			branches = x.XPath('json(*).data()[' .. i .. '].branches()')
+			local branches = x.XPath('json(*).data()[' .. i .. '].branches()')
 			for j = 1, branches.Count do
-				w = branches.Get(j)
+				local w = branches.Get(j)
 
-				bteam = x.XPath('json(*).data()[' .. i .. '].branches()[' .. j .. '].teams().name')
-				teams = {}
+				local bteam = x.XPath('json(*).data()[' .. i .. '].branches()[' .. j .. '].teams().name')
+				local teams = {}
 				for team = 1, bteam.Count do
 					teams[team] = bteam.Get(team).ToString()
 				end
 
-				bid        = w.GetProperty('branch_id').ToString()
-				name       = v.GetProperty('name').ToString()
-				volume     = v.GetProperty('volume').ToString()
-				chapter    = v.GetProperty('number').ToString()
-				scanlators = ' [' .. table.concat(teams, ", ") .. ']'
+				local bid = w.GetProperty('branch_id').ToString()
+				local name = v.GetProperty('name').ToString()
+				local volume = v.GetProperty('volume').ToString()
+				local chapter = v.GetProperty('number').ToString()
+				local scanlators = ' [' .. table.concat(teams, ", ") .. ']'
 
 				bid = bid ~= 'null' and bid ~= '' and string.format('&branch_id=%s', bid) or ''
 				name = name ~= 'null' and name ~= '' and string.format(' - %s', name) or ''
@@ -111,28 +109,39 @@ function _M.GetInfo()
 		end
 	end
 
+	HTTP.Reset()
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL
+
 	return no_error
 end
 
 -- Get the page count for the current chapter.
 function _M.GetPageNumber()
-	local i, server, sel_svr, svr = nil
 	local s = '/constants?fields[]=imageServers'
 	local u = API_URL .. URL
 
 	if not HTTP.GET(API_URL .. s) then return false end
 
-	svr = {'main', 'secondary', 'compress'}
-	sel_svr = (MODULE.GetOption('svr') or 0) + 1
-	server = CreateTXQuery(HTTP.Document).XPathString('json(*).data.imageServers()[id="' .. svr[sel_svr] .. '" and site_ids="' .. SITE_ID .. '"].url') .. '/'
+	local svr = {'main', 'secondary', 'compress'}
+	local sel_svr = (MODULE.GetOption('svr') or 0) + 1
+	local server = CreateTXQuery(HTTP.Document).XPathString('json(*).data.imageServers()[id="' .. svr[sel_svr] .. '" and site_ids="' .. SITE_ID .. '"].url') .. '/'
 
 	HTTP.Reset()
 	HTTP.Headers.Values['Authorization'] = MODULE.GetOption('auth')
+
 	if not HTTP.GET(u) then return false end
 
-	for i in CreateTXQuery(HTTP.Document).XPath('json(*).data.pages()').Get() do
-		TASK.PageLinks.Add(server .. i.GetProperty('url').ToString())
+	local x = CreateTXQuery(HTTP.Document)
+	for pages in x.XPath('json(*).data.pages()').Get() do
+		TASK.PageLinks.Add(server .. pages.GetProperty('url').ToString():gsub('^//', ''))
 	end
+
+	return true
+end
+
+-- Prepare the URL, http header and/or http cookies before downloading an image.
+function _M.BeforeDownloadImage()
+	HTTP.Headers.Values['Referer'] = MODULE.RootURL
 
 	return true
 end
