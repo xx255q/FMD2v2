fa10092806302f1e82b6ca7ab38ca858ef35c576
Slasar41 <agungtrianto41@outlook.com>
2025-08-06 22:34:53 +0700

    LectorManga: Fixed all (#2430)

--- lua/modules/LectorManga.lua
+++ lua/modules/LectorManga.lua
@@ -1,83 +1,53 @@
-function GetInfo()
-	if URL:find('/gotobook/',1,true) then
-		HTTP.Headers.Values['Referer'] = MODULE.RootURL .. '/library'
-	end
-	MANGAINFO.URL=MaybeFillHost(MODULE.RootURL,URL):gsub('/+$', '') .. '/'
-	if not HTTP.GET(MANGAINFO.URL) then return net_problem end
-	MANGAINFO.URL=HTTP.LastURL
-	x = CreateTXQuery(HTTP.Document)
-	MANGAINFO.Title     = x.XPathString('//h1/text()')
-	MANGAINFO.CoverLink = x.XPathString('//img[@class="img-fluid"]/@src')
-	MANGAINFO.Summary   = x.XPathString('//div/p')
-	MANGAINFO.Genres    = x.XPathStringAll('//a[contains(@href, "/library?genders")]')
-
-	local function parseChapters(s)
-		local v, y
-		local ctitle, stitle, title, link
-		for v in x.XPath('//div[@id="chapters'..s..'"]/div[@class="row"]').Get() do
-			ctitle = x.XPathString('./div/h4[contains(@class,"text-truncate")]', v)
-			for y in x.XPath('./following-sibling::ul[1]/li', v).Get() do
-				link = x.XPathString('.//a[contains(@href, "/view_uploads/")]/@href', y)
-				stitle = x.XPathString('./div/div[contains(@class,"text-truncate")]/span', y)
-				title = ctitle
-				if stitle ~= '' then title = title .. ' [' .. stitle .. ']' end
-				MANGAINFO.ChapterLinks.Add(link)
-				MANGAINFO.ChapterNames.Add(title)
-			end
-		end
-	end
-	parseChapters('')
-	parseChapters('-collapsed')
-
-	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
-	return no_error
-end
-
-function GetPageNumber()
-	if not HTTP.GET(MaybeFillHost(MODULE.RootURL, URL)) then return false end
-	local url = HTTP.LastURL
-	if url:find('/paginated') then
-		url = url:gsub('/paginated.*$', '/cascade')
-		HTTP.Reset()
-		HTTP.Headers.Add('Referer: '..HTTP.LastURL)
-		if not HTTP.GET(url) then return false end
-	end
-	CreateTXQuery(HTTP.Document).XPathStringAll('//img[contains(@class,"viewer-image")]/@data-src', TASK.PageLinks)
-	return true
-end
-
-function GetDirectoryPageNumber()
-	-- guessing the last directory page
-	PAGENUMBER = 990
-	return no_error
-end
-
-function GetNameAndLink()
-	local url = MODULE.RootURL .. '/library?order_item=creation&order_dir=desc'
-	if URL ~= '0' then url = url .. '&page=' .. (URL + 1) end
-	if not HTTP.GET(url) then return net_problem end
-	local x = CreateTXQuery(HTTP.Document)
-	x.XPathHREFTitleAll('//a[contains(@href,"/gotobook/")]', LINKS, NAMES)
-	local p = x.XPathString('//a[@rel="next"]/@href/substring-after(.,"&page=")')
-	if p ~= '' then
-		p = tonumber(p) or 0
-		-- update the last directory page if found
-		if p > UPDATELIST.CurrentDirectoryPageNumber then
-			UPDATELIST.CurrentDirectoryPageNumber = p
-		end
-	end
-	return no_error
-end
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
 
 function Init()
 	local m = NewWebsiteModule()
 	m.ID                       = 'd68542df8f5b47bbabcab226957598de'
-	m.Category                 = 'Spanish'
 	m.Name                     = 'LectorManga'
-	m.RootURL                  = 'https://lectormanga.com'
+	m.RootURL                  = 'https://lectormangaa.com'
+	m.Category                 = 'Spanish'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
 	m.OnGetInfo                = 'GetInfo'
 	m.OnGetPageNumber          = 'GetPageNumber'
-	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
-	m.OnGetNameAndLink         = 'GetNameAndLink'
-	m.SortedList               = True
+end
+
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local Template = require 'templates.Madara'
+XPathTokenStatus = 'Estado'
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get links and names from the manga list of the current website.
+function GetNameAndLink()
+	local u = MODULE.RootURL .. DirectoryPagination .. (URL + 1) .. DirectoryParameters
+
+	if not HTTP.GET(u) then return net_problem end
+
+	local x = CreateTXQuery(HTTP.Document)
+	if x.XPath('//div[contains(@class, "post-title")]/h3/a').Count == 0 then return no_error end
+	x.XPathHREFAll('//div[contains(@class, "post-title")]/h3/a', LINKS, NAMES)
+	UPDATELIST.CurrentDirectoryPageNumber = UPDATELIST.CurrentDirectoryPageNumber + 1
+
+	return no_error
+end
+
+-- Get info and chapter list for the current manga.
+function GetInfo()
+	Template.GetInfo()
+
+	return no_error
+end
+
+-- Get the page count for the current chapter.
+function GetPageNumber()
+	Template.GetPageNumber()
+
+	return true
 end
--- lua/templates/Madara.lua
+++ lua/templates/Madara.lua
@@ -44,7 +44,6 @@ end
 
 -- Get links and names from the manga list of the current website.
 function _M.GetNameAndLink()
-	local x = nil
 	local s = 'action=madara_load_more&template=madara-core/content/content-archive&page=' .. URL .. '&vars[paged]=0&vars[post_type]=wp-manga&vars[posts_per_page]=250'
 	local u = MODULE.RootURL .. '/wp-admin/admin-ajax.php'
 	HTTP.Headers.Values['Referer'] = u
@@ -52,7 +51,7 @@ function _M.GetNameAndLink()
 
 	if not HTTP.POST(u, s) then return net_problem end
 
-	x = CreateTXQuery(HTTP.Document)
+	local x = CreateTXQuery(HTTP.Document)
 	if x.XPath('//div[contains(@class, "post-title")]/*[self::h5 or self::h3]/a').Count == 0 then return no_error end
 	x.XPathHREFAll('//div[contains(@class, "post-title")]/*[self::h5 or self::h3]/a', LINKS, NAMES)
 	UPDATELIST.CurrentDirectoryPageNumber = UPDATELIST.CurrentDirectoryPageNumber + 1
@@ -62,19 +61,18 @@ end
 
 -- Get info and chapter list for the current manga.
 function _M.GetInfo()
-	local id, s, x = nil
 	local u = MaybeFillHost(MODULE.RootURL, URL)
 
 	if not HTTP.GET(u) then return net_problem end
 
-	x = CreateTXQuery(HTTP.Document)
+	local x = CreateTXQuery(HTTP.Document)
 	MANGAINFO.Title     = x.XPathString('//div[@class="post-title" or @id="manga-title"]/*[self::h1 or self::h3]/text()')
-	MANGAINFO.AltTitles = x.XPathString('//div[@class="summary-heading" and contains(./h5, "Alternative") or contains(./h5, "Judul Lain") or contains(./h5, "أسم بديل")]/following-sibling::div')
+	MANGAINFO.AltTitles = x.XPathString('//div[@class="summary-heading" and contains(./h5, "Alternativ") or contains(./h5, "Judul Lain") or contains(./h5, "أسم بديل")]/following-sibling::div')
 	MANGAINFO.CoverLink = x.XPathString('//div[@class="summary_image"]//img/@data-src')
 	MANGAINFO.Authors   = x.XPathStringAll('//div[@class="author-content"]/a')
 	MANGAINFO.Artists   = x.XPathStringAll('//div[@class="artist-content"]/a')
 	MANGAINFO.Genres    = x.XPathStringAll('//div[@class="genres-content"]/a')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//div[@class="summary-heading" and contains(., "' .. XPathTokenStatus .. '")]/following-sibling::div'), 'Berjalan|Ongoing|مستمرة|Em Andamento', 'Tamat|Completed|مكتملة|Concluído', 'Hiatus|On Hold|متوقفة|Em espera', 'Diberhentikan|Canceled|مُلغَاة|Cancelado')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//div[@class="summary-heading" and contains(., "' .. XPathTokenStatus .. '")]/following-sibling::div'), 'Berjalan|Ongoing|مستمرة|Em Andamento|En curso', 'Tamat|Completed|مكتملة|Concluído|Terminado', 'Hiatus|On Hold|متوقفة|Em espera|En espera', 'Diberhentikan|Canceled|مُلغَاة|Cancelado')
 	MANGAINFO.Summary   = x.XPathString('string-join(//div[contains(@class, "summary__content") or @class="manga-summary"]|//div[@class="manga-excerpt"]|//div[@class="post-content_item" and contains(h5, "Summary") or contains(h5, "Sinopsis")]//p, "\r\n")')
 
 	if MANGAINFO.CoverLink == '' then MANGAINFO.CoverLink = x.XPathString('//div[@class="summary_image"]//img/@src') end
@@ -82,14 +80,14 @@ function _M.GetInfo()
 	if MANGAINFO.Artists == '' then MANGAINFO.Artists = x.XPathString('//div[@class="summary-heading" and contains(., "' .. XPathTokenArtists .. '")]/following-sibling::div|//div[@class="manga-artists"]/a') end
 	if MANGAINFO.Genres == '' then MANGAINFO.Genres = x.XPathStringAll('//div[@class="summary-heading" and contains(., "' .. XPathTokenGenres .. '")]/following-sibling::div/a') end
 
-	id = x.XPathString('//div[contains(@id, "manga-chapters-holder")]/@data-id')
+	local id = x.XPathString('//div[contains(@id, "manga-chapters-holder")]/@data-id')
 	x.XPathHREFAll('//li[contains(@class, "wp-manga-chapter")]/a[not(@href="#")]', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
 	if MANGAINFO.ChapterLinks.Count == 0 then
 		HTTP.Reset()
 		HTTP.Headers.Values['Content-Length'] = 0
 		HTTP.Headers.Values['X-Requested-With'] = 'XMLHttpRequest'
 		if HTTP.POST(MANGAINFO.URL .. 'ajax/chapters') then
-			x = CreateTXQuery(HTTP.Document)
+			local x = CreateTXQuery(HTTP.Document)
 			x.XPathHREFAll('//div[@class="li__text"]/a[not(@href="#")]', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
 			if MANGAINFO.ChapterLinks.Count == 0 then
 				x.XPathHREFAll('//li[contains(@class, "wp-manga-chapter")]/a[not(@href="#")]', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
@@ -101,7 +99,7 @@ function _M.GetInfo()
 		HTTP.Headers.Values['Cache-Control'] = 'no-cache'
 		HTTP.Headers.Values['X-Requested-With'] = 'XMLHttpRequest'
 		HTTP.MimeType = 'application/x-www-form-urlencoded'
-		s = ChapterParameters .. id
+		local s = ChapterParameters .. id
 		if HTTP.POST(MODULE.RootURL .. '/wp-admin/admin-ajax.php', s) then
 			CreateTXQuery(HTTP.Document).XPathHREFAll('//li[contains(@class, "wp-manga-chapter")]/a[not(@href="#")]', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
 		end
@@ -113,13 +111,12 @@ end
 
 -- Get the page count for the current chapter.
 function _M.GetPageNumber()
-	local i, img, result, script, x = nil
 	local u = MaybeFillHost(MODULE.RootURL, URL)
 	if not u:find('style=list', 1, true) then u = u:gsub('?style=paged', '') .. '?style=list' end
 
 	if not HTTP.GET(u) then return false end
 
-	x = CreateTXQuery(HTTP.Document)
+	local x = CreateTXQuery(HTTP.Document)
 	x.XPathStringAll('//div[contains(@class, "page-break")]/img/@data-src', TASK.PageLinks)
 	if TASK.PageLinks.Count == 0 then x.XPathStringAll('//div[contains(@class, "page-break")]/img/@src', TASK.PageLinks) end
 	if TASK.PageLinks.Count == 0 then
@@ -127,18 +124,18 @@ function _M.GetPageNumber()
 		x.XPathStringAll('json(*)().src', TASK.PageLinks)
 	end
 	if TASK.PageLinks.Count == 0 then
-		x = CreateTXQuery(HTTP.Document)
-		script = x.XPathString('//script[@id="chapter-protector-data"]')
+		local x = CreateTXQuery(HTTP.Document)
+		local script = x.XPathString('//script[@id="chapter-protector-data"]')
 
 		if script == '' and MODULE.Storage['fullpageload'] ~= '' then
-			result = require 'utils.nodejs'.run_html_load(string.gsub(u, '?style=list', ''))
+			local result = require 'utils.nodejs'.run_html_load(string.gsub(u, '?style=list', ''))
 
 			x.ParseHTML(result)
 			script = x.XPathString('//script[@id="chapter-protector-data"]')
 		end
 
 		if script ~= '' then
-			img = require 'fmd.duktape'.ExecJS(script .. [[
+			local img = require 'fmd.duktape'.ExecJS(script .. [[
 
 			var CryptoJS = require("utils/crypto-js.min.js");
 			var CryptoJSAesJson = require("utils/cryptojs-aes-format.js");
