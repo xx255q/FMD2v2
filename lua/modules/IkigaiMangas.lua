816b025cfe1150b223d9229919715449dfe47670
Slasar41 <agungtrianto41@outlook.com>
2025-08-08 20:34:16 +0700

    Ikigai Mangas: Changed domain (#2437)

--- lua/modules/IkigaiMangas.lua
+++ lua/modules/IkigaiMangas.lua
@@ -6,7 +6,7 @@ function Init()
 	local m = NewWebsiteModule()
 	m.ID                       = 'ds42a85566244b7e836679491ce679e8'
 	m.Name                     = 'Ikigai Mangas'
-	m.RootURL                  = 'https://ikigaitoon.bakenen.com'
+	m.RootURL                  = 'https://visualikigai.kennyhowes.com'
 	m.Category                 = 'Spanish'
 	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
 	m.OnGetNameAndLink         = 'GetNameAndLink'
@@ -19,8 +19,8 @@ end
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-API_URL = 'https://panel.ikigaimangas.com/api/swf'
-DirectoryPagination = '/series?type=comic&nsfw=true&direction=desc&column=created_at&page='
+local API_URL = 'https://panel.ikigaimangas.com/api/swf'
+local DirectoryPagination = '/series?type=comic&nsfw=true&direction=desc&column=created_at&page='
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
@@ -39,7 +39,6 @@ end
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
-	local v = nil
 	local u = API_URL .. DirectoryPagination .. (URL + 1)
 
 	if not HTTP.GET(u) then return net_problem end
@@ -54,26 +53,25 @@ end
 
 -- Get info and chapter list for the current manga.
 function GetInfo()
-	local chapter, pages, title, v, x = nil
-	local u = API_URL .. '/series/' .. URL:match('/series/(.-)$')
-	local page = 1
+	local u = API_URL .. URL:match('(/series/.-)$')
 	
 	if not HTTP.GET(u) then return net_problem end
 	
-	x = CreateTXQuery(require 'fmd.crypto'.HTMLEncode(HTTP.Document.ToString()))
+	local x = CreateTXQuery(require 'fmd.crypto'.HTMLEncode(HTTP.Document.ToString()))
 	MANGAINFO.Title     = x.XPathString('json(*).series.name')
 	MANGAINFO.CoverLink = x.XPathString('json(*).series.cover')
 	MANGAINFO.Genres    = x.XPathStringAll('json(*).series.genres().name')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('json(*).series.status.name'), 'En Curso|Hiatus', 'Abandonada|Cancelada|Completa')
-	summary = x.XPathString('json(*).series.summary')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('json(*).series.status.name'), 'En Curso', 'Completa', 'Hiatus', 'Abandonada|Cancelada')
+	local summary = x.XPathString('json(*).series.summary')
 	if summary ~= 'null' then MANGAINFO.Summary = summary end
 
+	local page = 1
 	while true do
 		if not HTTP.GET(u .. 'chapters?page=' .. tostring(page)) then return net_problem end
-		x = CreateTXQuery(HTTP.Document)
+		local x = CreateTXQuery(HTTP.Document)
 		for v in x.XPath('json(*).data()').Get() do
-			chapter = x.XPathString('name', v)
-			title = x.XPathString('title', v)
+			local chapter = x.XPathString('name', v)
+			local title = x.XPathString('title', v)
 
 			title = title ~= 'null' and title ~= '' and string.format(' - %s', title) or ''
 
@@ -81,7 +79,7 @@ function GetInfo()
 			MANGAINFO.ChapterNames.Add('Capítulo ' .. chapter .. title)
 		end
 		page = page + 1
-		pages = tonumber(x.XPathString('json(*).meta.last_page')) or 1
+		local pages = tonumber(x.XPathString('json(*).meta.last_page')) or 1
 		if page > pages then
 			break
 		end
@@ -93,11 +91,11 @@ end
 
 -- Get the page count for the current chapter.
 function GetPageNumber()
-	local u = MODULE.RootURL .. '/capitulo' .. URL
+	local u = MODULE.RootURL .. '/capitulo' .. URL .. '/'
 
-	if not HTTP.GET(u) then return net_problem end
+	if not HTTP.GET(u) then return false end
 
 	CreateTXQuery(HTTP.Document).XPathStringAll('//div[contains(@class, "img")]/img/@src', TASK.PageLinks)
 
-	return no_error
+	return true
 end
