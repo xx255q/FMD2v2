0f4e79f1ecf6feec8b0ea9034e4fd64ee44eaa5d
Slasar41 <agungtrianto41@outlook.com>
2025-08-10 01:39:42 +0700

    Flame Comics: Filter ad images (#2442)

--- lua/modules/FlameComics.lua
+++ lua/modules/FlameComics.lua
@@ -17,7 +17,7 @@ end
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-DirectoryPagination = '/browse'
+local DirectoryPagination = '/browse'
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
@@ -25,7 +25,6 @@ DirectoryPagination = '/browse'
 
 -- Get links and names from the manga list of the current website.
 function GetNameAndLink()
-	local v = nil
 	local u = MODULE.RootURL .. DirectoryPagination
 
 	if not HTTP.GET(u) then return net_problem end
@@ -40,24 +39,24 @@ end
 
 -- Get info and chapter list for current manga.
 function GetInfo()
-	local chapter, json, title, v, x = nil
 	local u = MaybeFillHost(MODULE.RootURL, URL)
 
 	if not HTTP.GET(u) then return net_problem end
 
-	x = CreateTXQuery(HTTP.Document)
-	json = x.XPath('json(//script[@id="__NEXT_DATA__"]).props.pageProps.series')
+	local x = CreateTXQuery(HTTP.Document)
+	local json = x.XPath('json(//script[@id="__NEXT_DATA__"]).props.pageProps.series')
 	MANGAINFO.Title     = x.XPathString('title', json)
+	MANGAINFO.AltTitles = x.XPathString('altTitles', json):gsub('%[', ''):gsub('%]', ''):gsub('"', ''):gsub('%s+%,%s+', ', ')
 	MANGAINFO.CoverLink = MODULE.RootURL .. x.XPathString('//img[@alt="Cover"]/@src')
 	MANGAINFO.Authors   = x.XPathString('author', json)
 	MANGAINFO.Artists   = x.XPathString('artist', json)
-	MANGAINFO.Genres    = x.XPathString('tags', json):gsub('%[', ''):gsub('%]', ''):gsub('"', '')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('status', json), 'Ongoing|Hiatus', 'Completed|Dropped')
+	MANGAINFO.Genres    = x.XPathString('string-join(tags?*, ", ")', json)
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('status', json), 'Ongoing', 'Completed', 'Hiatus', 'Dropped')
 	MANGAINFO.Summary   = x.XPathString('description', json)
 
 	for v in x.XPath('json(//script[@id="__NEXT_DATA__"]).props.pageProps.chapters()').Get() do
-		chapter = v.GetProperty('chapter').ToString():gsub('%.0', '')
-		title   = v.GetProperty('title').ToString()
+		local chapter = v.GetProperty('chapter').ToString():gsub('%.0', '')
+		local title   = v.GetProperty('title').ToString()
 
 		title = title ~= 'null' and title ~= '' and string.format(' - %s', title) or ''
 
@@ -71,13 +70,12 @@ end
 
 -- Get the page count for the current chapter.
 function GetPageNumber()
-	local i = nil
 	local u = MaybeFillHost(MODULE.RootURL, URL)
 
 	if not HTTP.GET(u) then return net_problem end
 
-	for i in CreateTXQuery(HTTP.Document).XPath('//div[@class="m_6d731127 mantine-Stack-root"]/img/@src').Get() do
-		TASK.PageLinks.Add(MaybeFillHost(MODULE.RootURL, i.ToString()))
+	for v in CreateTXQuery(HTTP.Document).XPath('//div[@class="m_6d731127 mantine-Stack-root"]/img[@alt!=""]/@src').Get() do
+		TASK.PageLinks.Add(MaybeFillHost(MODULE.RootURL, v.ToString()))
 	end
 
 	return no_error
