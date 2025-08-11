40db6a4110807e01da818d518f2e629a8c927d94
Slasar41 <agungtrianto41@outlook.com>
2025-08-11 14:28:25 +0700

    SoftKomik: Fixed all (#2447)

--- lua/modules/SoftKomik.lua
+++ lua/modules/SoftKomik.lua
@@ -1,70 +1,91 @@
-local API_URL = 'https://img.softkomik.online/api'
-local CDN_URL = 'https://cdn.statically.io/img/softkomik.online'
+----------------------------------------------------------------------------------------------------
+-- Module Initialization
+----------------------------------------------------------------------------------------------------
 
 function Init()
 	local m = NewWebsiteModule()
-	m.ID                         = 'df01551e1739407a98669e37318842b0'
-	m.Name                       = 'SoftKomik'
-	m.RootURL                    = 'https://softkomik.site'
-	m.Category                   = 'Indonesian'
-	m.OnGetDirectoryPageNumber   = 'GetDirectoryPageNumber'
-	m.OnGetNameAndLink           = 'GetNameAndLink'
-	m.OnGetInfo                  = 'GetInfo'
-	m.OnGetPageNumber            = 'GetPageNumber'
+	m.ID                       = 'df01551e1739407a98669e37318842b0'
+	m.Name                     = 'SoftKomik'
+	m.RootURL                  = 'https://softkomik.com'
+	m.Category                 = 'Indonesian'
+	m.OnGetDirectoryPageNumber = 'GetDirectoryPageNumber'
+	m.OnGetNameAndLink         = 'GetNameAndLink'
+	m.OnGetInfo                = 'GetInfo'
+	m.OnGetPageNumber          = 'GetPageNumber'
 end
 
+----------------------------------------------------------------------------------------------------
+-- Local Constants
+----------------------------------------------------------------------------------------------------
+
+local API_URL = 'https://v2.softkomik.com/komik'
+local CDN_URL = 'https://image.softkomik.com/softkomik/'
+local DirectoryPagination = '/komik/list?page='
+
+----------------------------------------------------------------------------------------------------
+-- Event Functions
+----------------------------------------------------------------------------------------------------
+
+-- Get the page count of the manga list of the current website.
 function GetDirectoryPageNumber()
-	if HTTP.GET(API_URL .. '/komik-list?page=' .. '1') then
-		PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('json(*).DataByName.last_page')) or 1
-		return no_error
-	else
-		return net_problem
-	end
+	local u = MODULE.RootURL .. DirectoryPagination .. 1
+
+	if not HTTP.GET(u) then return net_problem end
+	
+	PAGENUMBER = tonumber(CreateTXQuery(HTTP.Document).XPathString('json(//script[@id="__NEXT_DATA__"]).props.pageProps.data.maxPage')) or 1
+
+	return no_error
 end
 
+-- Get links and names from the manga list of the current website.
 function GetNameAndLink()
-	if HTTP.GET(API_URL .. '/komik-list?page=' .. (URL + 1)) then
-		local x = CreateTXQuery(HTTP.Document)
-		local v for v in x.XPath('json(*).DataByName.data()').Get() do
-			LINKS.Add('/' .. x.XPathString('title_slug', v))
-			NAMES.Add(x.XPathString('title', v))
-		end
-		return no_error
-	else
-		return net_problem
+	local u = MODULE.RootURL .. DirectoryPagination .. (URL + 1)
+
+	if not HTTP.GET(u) then return net_problem end
+
+	for v in CreateTXQuery(HTTP.Document).XPath('json(//script[@id="__NEXT_DATA__"]).props.pageProps.data.data()').Get() do
+		LINKS.Add(v.GetProperty('title_slug').ToString())
+		NAMES.Add(v.GetProperty('title').ToString())
 	end
+
+	return no_error
 end
 
+-- Get info and chapter list for the current manga.
 function GetInfo()
-	local slug = URL:match('/(.-)$')
-	if HTTP.GET(API_URL .. '/lihat-komik/' .. slug) then
-		local x = CreateTXQuery(HTTP.Document)
-		MANGAINFO.Title      = x.XPathString('json(*).DataKomik.title')
-		MANGAINFO.CoverLink  = CDN_URL .. '/' .. x.XPathString('json(*).DataKomik.gambar')
-		MANGAINFO.Authors    = x.XPathString('json(*).DataKomik.author')
-		MANGAINFO.Genres     = x.XPathStringAll('json(*).dataGenre().nama_genre')
-		MANGAINFO.Status     = MangaInfoStatusIfPos(x.XPathString('json(*).DataKomik.status'))
-		MANGAINFO.Summary    = x.XPathString('json(*).DataKomik.sinopsis')
+	local u = API_URL .. URL
 
-		local v for v in x.XPath('json(*).DataChapter()').Get() do
-			MANGAINFO.ChapterLinks.Add(slug .. '&' .. x.XPathString('chapter', v))
-			MANGAINFO.ChapterNames.Add(x.XPathString('chapter', v))
-		end
-		MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
-		return no_error
-	else
-		return net_problem
+	if not HTTP.GET(u) then return net_problem end
+
+	local x = CreateTXQuery(HTTP.Document)
+	MANGAINFO.Title     = x.XPathString('json(*).title')
+	MANGAINFO.AltTitles = x.XPathString('json(*).title_alt')
+	MANGAINFO.CoverLink = 'https://softkomik.com/_next/image?url=https://cover.softkomik.com/softkomik-cover/' .. x.XPathString('json(*).gambar') .. '&w=256&q=100'
+	MANGAINFO.Authors   = x.XPathString('json(*).author')
+	MANGAINFO.Genres    = x.XPathStringAll('json(*).Genre()') .. ', ' .. x.XPathString('json(*).type'):gsub("^%l", string.upper)
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('json(*).status'))
+	MANGAINFO.Summary   = x.XPathString('json(*).sinopsis')
+
+	if not HTTP.GET(u .. '/chapter?limit=9999999') then return net_problem end
+
+	for v in CreateTXQuery(HTTP.Document).XPath('json(*).chapter()').Get() do
+		MANGAINFO.ChapterLinks.Add(URL .. '/chapter/' .. v.GetProperty('chapter').ToString())
+		MANGAINFO.ChapterNames.Add('Chapter ' .. v.GetProperty('chapter').ToString())
 	end
+	MANGAINFO.ChapterLinks.Reverse(); MANGAINFO.ChapterNames.Reverse()
+
+	return no_error
 end
 
+-- Get the page count for the current chapter.
 function GetPageNumber()
-	if HTTP.GET(API_URL .. '/baca-chapter' .. URL) then
-		local x = CreateTXQuery(HTTP.Document)
-		local v for v in x.XPath('json(*).DataGambar()').Get() do
-			TASK.PageLinks.Add(CDN_URL .. '/' .. x.XPathString('url_gambar', v))
-		end
-		return true
-	else
-		return false
+	local u = MaybeFillHost(MODULE.RootURL, URL)
+
+	if not HTTP.GET(u) then return false end
+
+	for v in CreateTXQuery(HTTP.Document).XPath('json(//script[@id="__NEXT_DATA__"]).props.pageProps.data.data.imageSrc()').Get() do
+		TASK.PageLinks.Add(CDN_URL .. v.ToString())
 	end
+
+	return true
 end
