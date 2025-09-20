e042ce92c4ca2d0282b676e96da0b360b70434c9
Slasar41 <agungtrianto41@outlook.com>
2025-09-20 09:45:44 +0700

    KeyoApp: Added missing manga info (#2522)

--- lua/templates/KeyoApp.lua
+++ lua/templates/KeyoApp.lua
@@ -8,7 +8,7 @@ local _M = {}
 -- Local Constants
 ----------------------------------------------------------------------------------------------------
 
-DirectoryPagination = '/series'
+local DirectoryPagination = '/series'
 
 ----------------------------------------------------------------------------------------------------
 -- Event Functions
@@ -27,19 +27,26 @@ end
 
 -- Get info and chapter list for the current manga.
 function _M.GetInfo()
-	local x = nil
 	local u = MaybeFillHost(MODULE.RootURL, URL)
 
 	if not HTTP.GET(u) then return net_problem end
 
-	x = CreateTXQuery(HTTP.Document)
+	local x = CreateTXQuery(HTTP.Document)
 	MANGAINFO.Title     = x.XPathString('//h1')
+	MANGAINFO.AltTitles = x.XPathString('//div[.="Alternative titles"]/following-sibling::div//span[not(contains(., "No alternative titles"))]')
 	MANGAINFO.CoverLink = x.XPathString('//div[contains(@class, "photoURL")]/@style'):match('--photoURL:url%((.-)%)')
 	MANGAINFO.Authors   = x.XPathString('//div[@alt="Author"]/span|//div[@class="grid gap-2 h-fit" and contains(., "Author")]/div[2]')
 	MANGAINFO.Artists   = x.XPathString('//div[@alt="Artist"]/span|//div[@class="grid gap-2 h-fit" and contains(., "Artist")]/div[2]')
 	MANGAINFO.Genres    = x.XPathStringAll('//a[contains(@href, "genre")]/span')
-	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//div[@alt="Status"]/span|//div[@class="grid gap-2 h-fit" and contains(., "Status")]/div[2]'), 'ongoing|paused', 'completed|dropped')
+	MANGAINFO.Status    = MangaInfoStatusIfPos(x.XPathString('//div[@alt="Status"]/span|//div[@class="grid gap-2 h-fit" and contains(., "Status")]/div[2]'), 'ongoing', 'completed', 'paused', 'dropped')
 	MANGAINFO.Summary   = x.XPathString('string-join(//div[@id="expand_content"]/p/text()|//div[@class="overflow-hidden"]/p/text(), "\r\n")')
+
+	local type = x.XPathString('//div[./div/span="Type"]/div[2]'):gsub('^%l', string.upper)
+	if MANGAINFO.Genres ~= '' then
+		MANGAINFO.Genres = MANGAINFO.Genres .. ', ' .. type
+	else
+		MANGAINFO.Genres = type
+	end
 
 	if MODULE.GetOption('showpaidchapters') then
 		x.XPathHREFTitleAll('//div[@id="chapters"]/a[not(.//span="Upcoming")]', MANGAINFO.ChapterLinks, MANGAINFO.ChapterNames)
@@ -53,13 +60,12 @@ end
 
 -- Get the page count for the current chapter.
 function _M.GetPageNumber()
-	local CDN_URL, v, x = nil
 	local u = MaybeFillHost(MODULE.RootURL, URL)
 
 	if not HTTP.GET(u) then return net_problem end
 
-	x = CreateTXQuery(HTTP.Document)
-	CDN_URL = x.XPathString('//script[contains(., "realUrl")]/substring-before(substring-after(., "realUrl = `"), "${uid}")')
+	local x = CreateTXQuery(HTTP.Document)
+	local CDN_URL = x.XPathString('//script[contains(., "realUrl")]/substring-before(substring-after(., "realUrl = `"), "${uid}")')
 	for v in x.XPath('//div[@id="pages"]/img/@uid').Get() do
 		TASK.PageLinks.Add(CDN_URL .. v.ToString())
 	end
